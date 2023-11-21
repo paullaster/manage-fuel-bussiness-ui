1fafbea3d4240f8749e9d55f67da5f58e160065f
unknown <paullasterokoth98@gmail.com>
2023-11-21 05:31:13 +0300

    finish up pump data style

--- src/packages/dashboard/admin/modules/company/components/PumpData.jsx
+++ src/packages/dashboard/admin/modules/company/components/PumpData.jsx
@@ -4,10 +4,6 @@ import { MdAdd } from "react-icons/md"
 
 const PumpData = () => {
   const [number_of_pumps, set_number_of_pumps] = useState(0);
-
-  const handleAddValves = () => {
-    return <ValvesData />
-  };
   return (
     <div className="pump_information">
       <div className="number_of_pumps">
@@ -27,13 +23,12 @@ const PumpData = () => {
           [...Array(Number(number_of_pumps))].map((_, index) => {
             return (
               <div key={index + 1} className="valve_information">
-                {/* <div className="valves"> */}
                   <p className="pump_caption">Pump {index + 1} valves </p>
                   <div className= {'valve_type'}>
                     <input
                       key={index}
                       type={"checkbox"}
-                      name={`valve_type`}
+                      name={`valve_type_for_pump_${index + 1}`}
                       required
                       value={'Petrol'}
                       readOnly
@@ -41,13 +36,13 @@ const PumpData = () => {
                       id="Petrol"
                     />
                     <label htmlFor="Petrol">Petrol</label>
-                    <input type={'number'} placeholder={'How many?'} className = {'type_number'} />
+                    <input type={'number'} placeholder={'How many?'} className = {'type_number'} name={`number_petrol_valve_type_for_pump_${index + 1}`} />
                   </div>
                   <div  className= {'valve_type'}>
                     <input
                       key={index}
                       type={"checkbox"}
-                      name={`valve_type`}
+                      name={`valve_type_for_pump_${index + 1}`}
                       required
                       value={'Diesel'}
                       readOnly
@@ -55,13 +50,13 @@ const PumpData = () => {
                       id="Diesel"
                     />
                     <label htmlFor="Diesel">Diesel</label>
-                    <input type={'number'} placeholder={'How many?'} className = {'type_number'} />
+                    <input type={'number'} placeholder={'How many?'} className = {'type_number'} name={`number_diesel_valve_type_for_pump_${index + 1}`} />
                   </div>
                   <div  className= {'valve_type'}>
                     <input
                       key={index}
                       type={"checkbox"}
-                      name={`valve_type`}
+                      name={`valve_type_for_pump_${index + 1}`}
                       required
                       value={'Kerosene'}
                       readOnly
@@ -69,23 +64,8 @@ const PumpData = () => {
                       className={'type_checkbox'}
                     />
                     <label htmlFor="Kerosene">Kerosene</label>
-                    <input htmlFor = "Kerosene" type={'number'} placeholder={'How many?'} className = {'type_number'} />
+                    <input htmlFor = "Kerosene" type={'number'} placeholder={'How many?'} className = {'type_number'}  name={`number_kerosene_valve_type_for_pump_${index + 1}`}/>
                   </div>
-                {/* </div> */}
-                {/* <Button
-                className={"btn-element btn_primary btn-icon"}
-                title={"add valve"}
-                onClick={handleAddValves}
-              >
-                <span><MdAdd size={30} /></span>
-                {/* <span>add valves</span> */}
-                {/* </Button> */}
-                {/* <InputComponent
-                type={'number'}
-                name={`tank_capacity_${index + 1}`}
-                placeholder={`Enter tank capacity for tank ${index + 1}`} required
-                min={1}
-              /> */}
               </div>
             )
           })

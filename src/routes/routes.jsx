87a4d3cdc15cdabfd545ce8558c65274e87e46be
unknown <paullasterokoth98@gmail.com>
2023-11-25 15:43:48 +0300

    adding auth UI

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -3,15 +3,21 @@ import App from '../App'
 import { ErrorComponent } from '../components';
 import { Landing } from '../packages/Landing';
 import { Admin, User, CompanyList, AddCompany, NewCompany, Wizard, TankAndPumpData } from '../packages/dashboard';
+import auth from '@/packages/auth';
+
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route element={<App />} errorElement={<ErrorComponent />} path='/'>
-            <Route element = {<Landing />} index/>
+            <Route element={<Landing />} index />
+            <Route element={<auth.View />} path='account'>
+                <Route element={<auth.components.Login />} path='login' />
+                <Route element={<auth.components.Register />} path='register' />
+            </Route>
             <Route element={<Admin />} path='admin/:id'>
                 <Route element={<CompanyList />} path='company/list' />
                 <Route element={<NewCompany />} path='company/new' action={AddCompany} />
-                <Route element={<Wizard /> } path='company/wizard/:step' action={TankAndPumpData}/>
+                <Route element={<Wizard />} path='company/wizard/:step' action={TankAndPumpData} />
             </Route>
             <Route element={<User />} path='user/:id'></Route>
         </Route>

768a32a2224ac4b1323991733b292c210e8b4d8b
unknown <paullasterokoth98@gmail.com>
2023-11-27 06:39:04 +0300

    logging page ui

--- src/packages/auth/components/Register.jsx
+++ src/packages/auth/components/Register.jsx
@@ -6,46 +6,50 @@ import keycloak from '@/assets/images/keycloak.svg';
 
 const Register = () => {
   return (
-      <>
-        <div>
-          <Form>
-            <InputComponent 
-            prelabelText = {'E-mail'}
-            placeholder = {'someone@email.com'}
-            type = {'email'}
-            required
-            >
+    <>
+      <div>
+        <Form>
+          <div className="input-group">
+            <label htmlFor="username">E-mail</label>
+            <div className="input_control">
+              <input
+                placeholder={'someone@email.com'}
+                type={'email'}
+                id='username'
+                required
+              />
               <MdAlternateEmail size={25} />
-            </InputComponent>
-            <InputComponent 
-            prelabelText = {'Password'}
-            placeholder = {'8+ characters, 1+ Capital letter, 1+ special characters'}
-            type = {'password'}
-            required
-            >
-              <MdLockOutline size = {25} />
-            </InputComponent>
-
-            <div>
-              <Button className = {'btn-element btn_primary'}>Create an account</Button>
             </div>
-          </Form>
+          </div>
+          <InputComponent
+            prelabelText={'Password'}
+            placeholder={'8+ characters, 1+ Capital letter, 1+ special characters'}
+            type={'password'}
+            required
+          >
+            <MdLockOutline size={25} />
+          </InputComponent>
+
           <div>
-          <Button className = {'btn-element btn_transparent'}>
+            <Button className={'btn-element btn_primary'}>Create an account</Button>
+          </div>
+        </Form>
+        <div>
+          <Button className={'btn-element btn_transparent'}>
             <span><img src={keycloak} alt='keycloack logo ' /></span>
             <span>Sign up with keycloak</span>
           </Button>
-          </div>
         </div>
-        <div>
-            <p>
-              <span>By proceeding you confirm you're in agreement with our</span>
-              <NavLink to = {'#'}>Privacy policy</NavLink>
-              <span>and</span>
-              <NavLink to = {'#'}>Terms</NavLink>
-              </p> 
-        </div>
-      </>
+      </div>
+      <div>
+        <p>
+          <span>By proceeding you confirm you're in agreement with our</span>
+          <NavLink to={'#'}>Privacy policy</NavLink>
+          <span>and</span>
+          <NavLink to={'#'}>Terms</NavLink>
+        </p>
+      </div>
+    </>
   )
 }
 

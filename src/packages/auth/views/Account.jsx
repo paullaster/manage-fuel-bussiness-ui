1a72a8d22ba4b0ceea901da109d3b3a4de9f545b
unknown <paullasterokoth98@gmail.com>
2023-11-27 06:25:39 +0300

    logging page ui

--- src/packages/auth/views/Account.jsx
+++ src/packages/auth/views/Account.jsx
@@ -4,7 +4,7 @@ import { Hide } from '@/utils';
 import { Logo } from '@/components';
 import { MdArrowDropDown } from 'react-icons/md';
 import { useEffect, useState } from 'react';
-import { v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 
 
 const Account = () => {
@@ -17,27 +17,27 @@ const Account = () => {
 
 
   const handleSelectLanguage = (e) => {
-     e.stopPropagation();
-     e.preventDefault();
-     setSelectLanguage(true);
+    e.stopPropagation();
+    e.preventDefault();
+    setSelectLanguage(true);
   }
 
   const languageList = [
-      {
-        id: uuidv4(),
-        lang: 'en',
-        caption: 'English',
-      },
-      {
-        id: uuidv4(),
-        lang: 'es',
-        caption: 'Espanyol',
-      },
-      {
-        id: uuidv4(),
-        lang: 'fr',
-        caption: 'French',
-      }
+    {
+      id: uuidv4(),
+      lang: 'en',
+      caption: 'English',
+    },
+    {
+      id: uuidv4(),
+      lang: 'es',
+      caption: 'Espanyol',
+    },
+    {
+      id: uuidv4(),
+      lang: 'fr',
+      caption: 'French',
+    }
   ]
 
   return (
@@ -52,54 +52,54 @@ const Account = () => {
           </div>
         </div>
         <div className='account_card_right'>
-          <div className='account_card_right_container'>
           <div className='i18n'>
-              <select id='i18n_list'>
-                {
-                  languageList.map((item) => {
-                    return (
-                      <option className = {selectLanguage && 'selectLangActive'} value= {item.lang} defaultValue={'en'}>{item.caption}</option>
-                    )
-                  })
-                }
-                {/* <option value="en" defaultValue={'en'}>English</option>
+            <select id='i18n_list'>
+              {
+                languageList.map((item) => {
+                  return (
+                    <option key = {item.id} className={selectLanguage && 'selectLangActive'} value={item.lang} defaultValue={'en'}>{item.caption}</option>
+                  )
+                })
+              }
+              {/* <option value="en" defaultValue={'en'}>English</option>
                 <option value="es">Espanol</option>
                 <option value="fr">French</option> */}
-              </select>
-              <span onClick={ handleSelectLanguage }>
-                <MdArrowDropDown size = {25} />
-              </span>
+            </select>
+            <span onClick={handleSelectLanguage}>
+              <MdArrowDropDown size={25} />
+            </span>
           </div>
-          <div className='conditional_account_page_text'>
-            {
-              Hide('/register') &&
-              <div className='free_package'>
-                <p>START FOR FREE</p>
+          <div className="account_specific">
+            <div className='conditional_account_page_text'>
+              {
+                Hide('/register') &&
+                <div className='free_package'>
+                  <p>START FOR FREE</p>
+                </div>
+              }
+              <div className='account_sign_options'>
+                <h2>
+                  {
+                    Hide('/register')
+                      ? 'Sign up to fluecloud'
+                      : Hide('/login')
+                        ? 'Sign in to fluecloud'
+                        : redirect('/*')
+                  }
+                </h2>
               </div>
-            }
-            <div className='account_sign_options'>
-              <h2>
+              <div className='account_action_option'>
                 {
-                  Hide('/register')
-                    ? 'Sign up to fluecloud'
-                    : Hide('/login')
-                      ? 'Sign in to fluecloud'
-                      : redirect('/*')
+                  Hide('/register') && <p>Already a member? <NavLink to={'/account/login'}>Login</NavLink></p>
                 }
-              </h2>
+                {
+                  Hide('/login') && <p>Do not have account yet? <NavLink to={'/account/register'}>Register</NavLink></p>
+                }
+              </div>
             </div>
-            <div className='account_action_option'>
-              {
-                Hide('/register') && <p>Already a member? <NavLink to={'/account/login'}>Login</NavLink></p>
-              }
-              {
-                Hide('/login') && <p>Do not have account yet? <NavLink to={'/account/register'}>Register</NavLink></p>
-              }
+            <div className='account_options'>
+              <Outlet />
             </div>
-          </div>
-          <div className='account_options'>
-            <Outlet />
-          </div>
           </div>
         </div>
       </div>

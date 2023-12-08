512d6d1bc08e253489f69c0d9a882947d7371844
unknown <paullasterokoth98@gmail.com>
2023-12-08 03:52:41 +0300

    update vendor index

--- src/packages/purchases/modules/vendors/views/Vendors.jsx
+++ src/packages/purchases/modules/vendors/views/Vendors.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import { MdChevronRight } from "react-icons/md";
+import vendorsImage from "@/assets/images/vendors.svg";
+import { NavLink } from "react-router-dom";
 
 const Vendors = () => {
   return (
-    <div>Vendors</div>
+    <section className="vendor_index">
+      <div className="vendor_index_actions">
+        <div className="vendor_heading">
+          <h2>Vendors</h2>
+          <p>
+            Vendors are required if you want to create bills. You can see the balance<br />you owe and filter reports by the vendor.
+          </p>
+        </div>
+        <div className="vendor_action_link">
+          <div>
+            <NavLink>
+              <span className="action_name_and_description">
+                <span className="action_name new_vendor_glow">new vendor</span>
+                <span className="action_description">Enter the details and create your first vendor</span>
+              </span>
+              <span className="action_graphic"><MdChevronRight size={30} /></span>
+            </NavLink>
+          </div>
+          <div>
+            <NavLink>
+              <span className="action_name_and_description">
+                <span className="action_name">import vendor</span>
+                <span className="action_description">Import your existing vendor with a single click</span>
+              </span>
+              <span className="action_graphic"><MdChevronRight size={30} /></span>
+            </NavLink>
+          </div>
+          <div>   
+            <NavLink>
+              <span className="action_name_and_description">
+                <span className="action_name">Payroll</span>
+                <span className="action_description">Reduce time spent on employee payments, benefits, and deductions</span>
+              </span>
+              <span className="action_graphic"><MdChevronRight size={30} /></span>
+            </NavLink>
+          </div>
+        </div>
+      </div>
+      <div className="vendor_graphics">
+        <img src={vendorsImage} alt="Vendors image" />
+      </div>
+    </section>
   )
 }
 

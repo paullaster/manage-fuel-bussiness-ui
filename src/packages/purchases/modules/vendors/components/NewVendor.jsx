c48651c698fd6d7bef90c01256b36395813257aa
unknown <paullasterokoth98@gmail.com>
2023-12-08 04:49:45 +0300

    update vendor index

--- src/packages/purchases/modules/vendors/components/NewVendor.jsx
+++ src/packages/purchases/modules/vendors/components/NewVendor.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import { Button, InputComponent,  } from "@/components";
+import { MdOutlineStarOutline } from "react-icons/md";
+import { Form } from "react-router-dom";
+
 
 const NewVendor = () => {
   return (
-    <div>NewVendor</div>
+    <section>
+        <div>
+            <div className="new_vendor_introduction">
+                <h2>New Vendor</h2>
+                <Button>
+                    <MdOutlineStarOutline size={35} />
+                </Button>
+            </div>
+            <div className="new_vendor_form">
+                <Form>
+                    <div>
+                        <label htmlFor="organization_id">Organization</label>
+                        <select name="organization_id" id="organization_id">
+
+                        </select>
+                    </div>
+                    <InputComponent 
+                    type="text"
+                    prela
+                    />
+                </Form>
+            </div>
+        </div>
+        <div></div>
+    </section>
   )
 }
 

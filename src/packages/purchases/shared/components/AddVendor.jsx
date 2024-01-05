5a5e1c8063147f74b0327552221663fc2ee51913
paullaster <paullasterokoth98@gmail.com>
2024-01-05 09:16:42 +0300

    refactor: refactoring creating new fuel purchase page ui

--- src/packages/purchases/shared/components/AddVendor.jsx
+++ src/packages/purchases/shared/components/AddVendor.jsx
@@ -10,20 +10,8 @@ const AddVendor = () => {
                   <MdPersonAdd size={20} />
               </span>
               <span>
-                  Add a vendor
+                  Add vendor
               </span>
-          </div>
-          <div className="add_new_item_card">
-              <div className="add_new_item_card__search">
-                  <span><MdOutlineSearch size={20} /></span>
-                  <input type="search" name="vendor" id="vendor" />
-              </div>
-              <div>
-              </div>
-              <Button>
-                  <span><MdAdd size={20} /></span>
-                  <span>new vendor</span>
-              </Button>
           </div>
       </div>
   )
---
+++ src/packages/purchases/shared/components/Autocomplete.jsx
@@ -0,0 +1,25 @@
+
+
+const Autocomplete = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Autocomplete
+
+
+    // < div className = "add_new_item_card" >
+    //           <div className="add_new_item_card__search">
+    //               <span><MdOutlineSearch size={20} /></span>
+    //               <input type="search" name="vendor" id="vendor" />
+    //           </div>
+    //           <div>
+    //           </div>
+    //           <Button>
+    //               <span><MdAdd size={20} /></span>
+    //               <span>new vendor</span>
+    //           </Button>
+    //       </div >

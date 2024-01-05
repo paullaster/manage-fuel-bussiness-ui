893fa63f36582fd39b8b208e51dcb1414aa45ef6
paullaster <paullasterokoth98@gmail.com>
2024-01-05 12:08:29 +0300

    refactor: refactoring adding vendor - creating reusable components

--- src/packages/purchases/shared/components/index.jsx
+++ src/packages/purchases/shared/components/index.jsx
@@ -2,12 +2,12 @@ import PurchasesItemsIndex from "./PurchasesItemsIndex";
 import { NewConstruct } from "./NewConstruct";
 import SectionIntroduction from "./SectionIntroduction";
 import AddItemButton from "./AddItemButton";
-import AddVendor from "./AddItem";
+import AddItem from "./AddItem";
 
 export default {
     PurchasesItemsIndex,
     NewConstruct,
     SectionIntroduction,
     AddItemButton,
-    AddVendor,
+    AddItem,
 }

315f2a75d12ab14e3e673a20115f0a3000f99f9b
paullaster <paullasterokoth98@gmail.com>
2024-01-06 01:38:59 +0300

    feature: adding form actions buttons

--- src/packages/purchases/modules/items/components/ItemComponent.jsx
+++ src/packages/purchases/modules/items/components/ItemComponent.jsx
@@ -2,22 +2,26 @@ import { InputComponent } from "@/components";
 const ItemComponent = () => {
     return (
         <div className="itemComponent">
-            <InputComponent
-                prelabelText="Item name"
-                name="item_name"
-            />
-            <fieldset className="input-group customfielset">
-                <legend>Description</legend>
-                <textarea name="description" id="description" cols="30" rows="5"></textarea>
-            </fieldset>
-            <InputComponent
-                prelabelText="Buying price"
-                name="buying_price"
-            />
-            <InputComponent
-                prelabelText="Selling price"
-                name="selling_price"
-            />
+            <div className="itemDetails">
+                <InputComponent
+                    prelabelText="Item name"
+                    name="item_name"
+                />
+                <fieldset className="input-group customfielset">
+                    <legend>Description</legend>
+                    <textarea name="description" id="description" cols="30" rows="5"></textarea>
+                </fieldset>
+            </div>
+            <div>
+                <InputComponent
+                    prelabelText="Buying price"
+                    name="buying_price"
+                />
+                <InputComponent
+                    prelabelText="Selling price"
+                    name="selling_price"
+                />
+            </div>
         </div>
     )
 }

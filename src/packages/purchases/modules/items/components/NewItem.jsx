1325b6afa3d4ab34f68ff110aab68985b40015a6
paullaster <paullasterokoth98@gmail.com>
2024-01-06 00:53:47 +0300

    refactor:refactor fuel purchase ui

--- src/packages/purchases/modules/items/components/NewItem.jsx
+++ src/packages/purchases/modules/items/components/NewItem.jsx
@@ -3,7 +3,20 @@ import { InputComponent } from "@/components";
 const NewItem = () => {
   return (
     <section className='purchaseItem'>
-
+      <div className="transport">
+        <InputComponent
+          prelabelText="transport name"
+          name="transport_name"
+        />
+        <InputComponent
+          prelabelText="vehicle registration"
+          name="vehicle_registration"
+        />
+        <InputComponent
+          prelabelText="driver name"
+          name="driver_name"
+        />
+      </div>
     </section>
   )
 }

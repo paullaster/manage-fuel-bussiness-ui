1ed9226a958fde37515dc86a1796ae3cbd5083f8
paullaster <paullasterokoth98@gmail.com>
2024-01-06 02:36:17 +0300

    Helpers: adding app helper file, AuthUtil

--- src/packages/purchases/modules/items/ItemFormAction.jsx
+++ src/packages/purchases/modules/items/ItemFormAction.jsx
@@ -5,10 +5,12 @@ import AuthUtils from "@/utils/AuthUtils";
 
 const saveNewPurchaseItem = async({request}) => {
     let data = Object.entries( await request.formData());
+    let sessionUser = AuthUtils.getItemFromStorage(`${APPNAME}_user`);
+    let orgId = sessionUser ? sessionUser.organization_id : new Error("The is no exsting session user!");
     data = {
         ...data,
-        organization_id: getItemFromStorage(`${APPNAME}_user`).organization_id,
+        organization_id: orgId,
     }
-    const response = await _request({ method: 'POST', url: constants.purchaseItem })
+    const response = await _request({ method: 'POST', url: constants.purchaseItem, data: data, headers: { 'Content-Type': 'application/json' } })
 
 }

4283dd107fe183fb681012c5adbdd6e7048cece3
paullaster <paullasterokoth98@gmail.com>
2024-01-06 01:09:49 +0300

    feature: adding form actions buttons

--- src/packages/purchases/modules/items/components/NewItem.jsx
+++ src/packages/purchases/modules/items/components/NewItem.jsx
@@ -1,10 +1,11 @@
 import { Form } from "react-router-dom";
 import ItemComponent from "./ItemComponent";
 import FormButtonRow from "./FormButtonRow";
+import shared from "../../../shared";
 const NewItem = () => {
   return (
     <section className='purchaseItem'>
-      
+      <shared.components.SectionIntroduction text="New purcahse item" />
       <Form>
         <ItemComponent />
         <FormButtonRow />

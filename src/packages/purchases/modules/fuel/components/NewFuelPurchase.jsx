38910e19fb267a6a8d094050cfecc4cc16a303d4
paullaster <paullasterokoth98@gmail.com>
2024-01-05 07:34:23 +0300

    refactor: refactoring creating new fuel purchase page ui

--- src/packages/purchases/modules/fuel/components/NewFuelPurchase.jsx
+++ src/packages/purchases/modules/fuel/components/NewFuelPurchase.jsx
@@ -91,29 +91,7 @@ const NewFuelPurchase = () => {
     <section className="newfuelpurchase">
       <shared.components.SectionIntroduction text="New Fuel Purchase" />
       <div className="newfuelpurchase_autofil">
-        <div className="">
-          <label htmlFor="vendor" className="label-required">Vendor</label>
-          <div className="add_item_placeholder">
-            <span>
-              <MdPersonAdd size={20} />
-            </span>
-            <span>
-              Add a vendor
-            </span>
-          </div>
-          <div className="add_new_item_card">
-            <div className="add_new_item_card__search">
-              <span><MdOutlineSearch size={20} /></span>
-              <input type="search" name="vendor" id="vendor" />
-            </div>
-            <div>
-            </div>
-            <Button>
-              <span><MdAdd size={20} /></span>
-              <span>new vendor</span>
-            </Button>
-          </div>
-        </div>
+        
         <div>
           <label htmlFor="invoice">product</label>
           <div>
--- src/packages/purchases/shared/components/AddVendor.jsx
+++ src/packages/purchases/shared/components/AddVendor.jsx
@@ -2,7 +2,29 @@ import React from 'react'
 
 const AddVendor = () => {
   return (
-    <div>AddVendor</div>
+      <div className="">
+          <label htmlFor="vendor" className="label-required">Vendor</label>
+          <div className="add_item_placeholder">
+              <span>
+                  <MdPersonAdd size={20} />
+              </span>
+              <span>
+                  Add a vendor
+              </span>
+          </div>
+          <div className="add_new_item_card">
+              <div className="add_new_item_card__search">
+                  <span><MdOutlineSearch size={20} /></span>
+                  <input type="search" name="vendor" id="vendor" />
+              </div>
+              <div>
+              </div>
+              <Button>
+                  <span><MdAdd size={20} /></span>
+                  <span>new vendor</span>
+              </Button>
+          </div>
+      </div>
   )
 }
 

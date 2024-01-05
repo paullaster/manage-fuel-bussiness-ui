930e49f42196a2bd93d8ed20690919d6ba8cb3cc
paullaster <paullasterokoth98@gmail.com>
2024-01-05 16:23:08 +0300

    create shared state reducer: style add item card

--- src/packages/purchases/shared/Sharedcontenxt.jsx
+++ src/packages/purchases/shared/Sharedcontenxt.jsx
@@ -1,4 +1,14 @@
 import { createContext, useContext, useReducer } from "react";
+import state from "./state";
+import { stateActions } from "./StateActions";
 
 const SharedStateContentxt = createContext(null);
 const SharedStateActioncontext = createContext(null);
+
+export const SharedStateProvider = ({children}) => {
+  const [sharedState, sharedStateAction] = useReducer(
+    stateActions,
+    state
+  );
+  return 
+}

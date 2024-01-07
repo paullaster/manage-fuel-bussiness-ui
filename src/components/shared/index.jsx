603746541b6a4926a7cf13e66e248313c70f59f8
paullaster <paullasterokoth98@gmail.com>
2024-01-07 07:52:18 +0300

    Date picker: Add date pincker

--- src/components/shared/index.jsx
+++ src/components/shared/index.jsx
@@ -7,3 +7,4 @@ export { default as TableComponent } from './TableComponent';
 export { default as PaginatorComponent } from './PaginatorComponent';
 export { default as InputComponent } from './InputComponent';
 export { default as Stepper} from './Stepper';
+export { default as DatePicker } from './DatePicker';

3f93e9c6528ba2f004fcc4e47f20cf452da254f3
paullaster <paullasterokoth98@gmail.com>
2024-01-07 07:53:18 +0300

    Date picker: Add date pincker

--- src/components/index.js
+++ src/components/index.js
@@ -1,14 +1,14 @@
 import { ErrorComponent } from "./Error";
-import { 
-    Footer, Navigation, Sidebar, Button, 
-    SearchComponent, TableComponent, PaginatorComponent, 
-    InputComponent, Stepper 
+import {
+    Footer, Navigation, Sidebar, Button,
+    SearchComponent, TableComponent, PaginatorComponent,
+    InputComponent, Stepper, DatePicker
 } from './shared';
 
 
 export { default as Logo } from './Logo';
-export  { 
-    ErrorComponent, Footer, Navigation, 
+export {
+    ErrorComponent, Footer, Navigation,
     Sidebar, Button, SearchComponent, TableComponent,
-     PaginatorComponent, InputComponent, Stepper 
-    };
+    PaginatorComponent, InputComponent, Stepper, DatePicker
+};

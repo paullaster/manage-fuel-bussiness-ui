aa1ffb78c7c12262bfade7d68901a5a5fa280478
paullaster <paullasterokoth98@gmail.com>
2024-01-07 15:41:58 +0300

    style :adding style for billing component

--- src/packages/purchases/modules/fuel/components/InvoiceDetails.jsx
+++ src/packages/purchases/modules/fuel/components/InvoiceDetails.jsx
@@ -7,17 +7,17 @@ const InvoiceDetails = () => {
                 prelabelText="Invoice number"
                 name="invoice_number"
             />
-            <DatePickerComponent label="Invoice date"/>
             <InputComponent
                 prelabelText="Purchase order number"
                 name="po_number"
             />
-            <DatePickerComponent label="Purchase order date"/>
             <InputComponent
                 prelabelText="Delivery note number"
                 name="delivery_note_number"
             />
+            <DatePickerComponent label="Invoice date"/>
             <DatePickerComponent label="Delivery note date"/>
+            <DatePickerComponent label="Purchase order date"/>
         </div>
     )
 }

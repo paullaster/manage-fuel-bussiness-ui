d693e2a478bc5d38dea3d5d82b643f2c280d51db
paullaster <paullasterokoth98@gmail.com>
2024-01-07 18:28:24 +0300

    style: style the refactored fuel purchaseentry page

--- src/packages/purchases/modules/fuel/components/NewFuelPurchase.jsx
+++ src/packages/purchases/modules/fuel/components/NewFuelPurchase.jsx
@@ -1,14 +1,13 @@
 import { useEffect } from "react";
 import shared from "../../../shared";
 import { useGlobalDispatcher, useGlobalState } from '@/store';
-import Transport from "./Transport";
 import { Form } from "react-router-dom";
 import { composableAutofils } from "../setups";
 import { Button } from "@/components";
-import InvoiceDetails from "./InvoiceDetails";
 import TankEntries from "./TankEntries";
 import BillingComponent from "./BillingComponent";
 import TransportationAndOfficer from "./TransportationAndOfficer";
+import { MdOutlineSimCardDownload } from "react-icons/md";
 
 
 
@@ -26,13 +25,13 @@ const NewFuelPurchase = () => {
     <section className="newfuelpurchase">
       <shared.components.SectionIntroduction text="New Fuel Purchase" />
       <Form>
-          <BillingComponent cardLabelView={cardLabelView}/>
-          <TransportationAndOfficer cardLabelView={cardLabelView} />
-          <TankEntries />
-          <div className="form_actions">
-            <Button type="button" className={'btn-element'}> cancel </Button>
-            <Button type="submit" className={'btn-element btn_primary'}> save </Button>
-          </div>
+        <BillingComponent cardLabelView={cardLabelView} />
+        <TransportationAndOfficer cardLabelView={cardLabelView} />
+        <TankEntries />
+        <div className="form_actions">
+          <Button type="button" className={'btn-element'}> cancel </Button>
+          <Button type="submit" className={'btn-element btn_primary'}> <span><MdOutlineSimCardDownload size={20} /></span> <span>save</span></Button>
+        </div>
       </Form>
     </section>
   )

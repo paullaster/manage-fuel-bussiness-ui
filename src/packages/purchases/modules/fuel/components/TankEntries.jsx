934f59a43f4d0a7144fe83698c42278696744c76
paullaster <paullasterokoth98@gmail.com>
2024-01-07 15:01:59 +0300

    component:creating resuable tank component

--- src/packages/purchases/modules/fuel/components/TankEntries.jsx
+++ src/packages/purchases/modules/fuel/components/TankEntries.jsx
@@ -1,17 +1,13 @@
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-
+import FuelPurchaseEntry from "./FuelPurchaseEntry"
 const TankEntries = () => {
     return (
         <div>
-            <AppBar position='static'>
-                <Toolbar>
-                    <Typography variant="h6" color="inherit" component="h4">
-                        Tank Entries
-                    </Typography>
-                </Toolbar>
-            </AppBar>
+            <div>
+
+            </div>
+            <div>
+            <FuelPurchaseEntry />
+            </div>
         </div>
     )
 }

dbcbcdf44d4cc920fda0e15527b109bc80501667
paullaster <paullasterokoth98@gmail.com>
2024-01-07 07:30:53 +0300

    refactor ui: refactoring fuel purchase ui

--- src/components/shared/DataTable.jsx
+++ src/components/shared/DataTable.jsx
@@ -1,15 +1,23 @@
 import { DataGrid } from '@mui/x-data-grid';
 
-const DataTable = ({ rows, columns, initialState: {} }) => {
-  return (
-    <div>
-        <DataGrid 
-        rows={rows}
-        columns={columns}
-        initialState={initialState}
-        />
-    </div>
-  )
+const DataTable = ({
+    rows,
+    columns,
+    initialState = { pagination: { paginationModel: { page: 0, pageSize: 5 }, } },
+    pageSizeOptions = [5, 10],
+    ...args
+}) => {
+    return (
+        <div>
+            <DataGrid
+                rows={rows}
+                columns={columns}
+                initialState={initialState}
+                pageSizeOptions={pageSizeOptions}
+                {...args}
+            />
+        </div>
+    )
 }
 
 export default DataTable

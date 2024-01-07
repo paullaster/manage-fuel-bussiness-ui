7c50b8632c2abb31ce8174bd9a7409da8d68a279
paullaster <paullasterokoth98@gmail.com>
2024-01-07 14:52:14 +0300

    component:creating resuable tank component

--- src/packages/purchases/modules/fuel/components/TankEntries.jsx
+++ src/packages/purchases/modules/fuel/components/TankEntries.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
 
 const TankEntries = () => {
   return (
     <div>
-       
+       <AppBar position='static'>
+            <Toolbar>
+                <h4>Tank entries</h4>
+            </Toolbar>
+       </AppBar>
     </div>
   )
 }

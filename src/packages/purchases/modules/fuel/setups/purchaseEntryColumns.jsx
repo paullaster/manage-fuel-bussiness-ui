4b32d13919273204103f72a51abb28c7d9133e7e
paullaster <paullasterokoth98@gmail.com>
2024-01-07 14:18:03 +0300

    update: fuel purchase entry

--- src/packages/purchases/modules/fuel/setups/purchaseEntryColumns.jsx
+++ src/packages/purchases/modules/fuel/setups/purchaseEntryColumns.jsx
@@ -6,27 +6,37 @@ export default [
         editable: true,
     },
     {
+        field: 'fuel_type',
+        headerName: 'Fuel Type',
+        width: 150,
+        editable: false,
+        sortable: false,
+        valueGetter: (params) => {
+            
+        }
+    },
+    {
         field: 'dip_quantity_before_offloading',
         headerName: 'Dip quantity before offloading',
-        width: 200,
+        width: 210,
         editable: true,
     },
     {
         field: 'sales_quantity_during_offloading',
         headerName: 'Sales quantity during offloading',
-        width: 200,
+        width: 210,
         editable: true,
     },
     {
         field: 'actual_dip_quantity_after_offloading',
         headerName: 'Actual dip quantity after offloading',
-        width: 200,
+        width: 210,
         editable: true,
     },
     {
         field: 'expected_quantity',
         headerName: 'Expected quantity',
-        width: 200,
+        width: 210,
         editable: true,
     },
     {
@@ -53,7 +63,7 @@ export default [
         headerName: 'Amount',
         description: 'Derived amount',
         sortable: false,
-        width: 150,
+        width: 100,
         valueGetter: (params) => {
             return Number(params.row.expected_quantity)  || 0 * Number(params.row.price) || 0;
         }

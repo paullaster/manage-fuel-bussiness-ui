189213cfad3a943855a62be9ccd14961365f3d38
paullaster <paullasterokoth98@gmail.com>
2024-01-07 15:57:40 +0300

    refactor :adding reusable billing component

--- src/packages/purchases/modules/fuel/components/BillingComponent.jsx
+++ src/packages/purchases/modules/fuel/components/BillingComponent.jsx
@@ -3,13 +3,15 @@ import shared from "../../../shared";
 
 const BillingComponent = ({ cardLabelView }) => {
     return (
-        <div className="billing">
-            {
-                cardLabelView.length && cardLabelView.slice(0, 1).map((card) => {
-                    return <shared.components.AddItem label={card.name} cardLabelIcon={card.CustomCardLabelIcon} key={card.card} cardView={card.cardView} addItemView={card.addItemView} id={card.card} />
-                })
-            }
-            <InvoiceDetails />
+        <div>
+            <div className="billing">
+                {
+                    cardLabelView.length && cardLabelView.slice(0, 1).map((card) => {
+                        return <shared.components.AddItem label={card.name} cardLabelIcon={card.CustomCardLabelIcon} key={card.card} cardView={card.cardView} addItemView={card.addItemView} id={card.card} />
+                    })
+                }
+                <InvoiceDetails />
+            </div>
         </div>
     )
 }

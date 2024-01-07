a444b6eccfe162540e57b2946b718ad020bcaa3a
paullaster <paullasterokoth98@gmail.com>
2024-01-07 15:09:16 +0300

    component:creating resuable tank component

--- src/packages/purchases/modules/fuel/components/TankEntries.jsx
+++ src/packages/purchases/modules/fuel/components/TankEntries.jsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components"
 import FuelPurchaseEntry from "./FuelPurchaseEntry"
+import AddItemButton from "../../../shared/components/AddItemButton"
 const TankEntries = () => {
     return (
         <div>
             <div>
-
+                <div>Tank Entries</div>
+                <AddItemButton btnCaption="Add Tank Entry"/>
             </div>
             <div>
                 <FuelPurchaseEntry />

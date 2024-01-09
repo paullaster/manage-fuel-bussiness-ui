481669d70249768751c00d1af19240d0d936eef7
paullaster <paullasterokoth98@gmail.com>
2024-01-09 23:31:08 +0300

    refactor: add billing to shared

--- src/packages/purchases/modules/items/components/NewItem.jsx
+++ src/packages/purchases/modules/items/components/NewItem.jsx
@@ -1,7 +1,19 @@
+import { useEffect } from "react";
 import { Form } from "react-router-dom";
 import FormButtonRow from "./FormButtonRow";
 import shared from "../../../shared";
+import { useGlobalDispatcher, useGlobalState } from '@/store';
+import { composableAutofils } from "../setups";
+
 const NewItem = () => {
+  const appStateDispatcher = useGlobalDispatcher();
+  const { cardLabelView } = useGlobalState();
+
+  useEffect(() => {
+    appStateDispatcher({ type: "CREATECOMPOSABLEAUTOFILS", payload: composableAutofils });
+  }, []);
+
+
   return (
     <section className='purchaseItem'>
       <shared.components.SectionIntroduction text="New purchase item" />

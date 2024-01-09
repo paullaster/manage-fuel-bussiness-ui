b8c514fec9c6ab27d4c0f5fc1070d5af92223708
paullaster <paullasterokoth98@gmail.com>
2024-01-09 23:49:10 +0300

    refactor: add billing to shared

--- src/packages/purchases/modules/items/components/PurchaseItemEntry.jsx
+++ src/packages/purchases/modules/items/components/PurchaseItemEntry.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import FuelPurchaseEntry from "./FuelPurchaseEntry"
+import AddItemButton from "../../../shared/components/AddItemButton"
+import DivisionTopBar from "../../../shared/components/DivisionTopBar"
 
 const PurchaseItemEntry = () => {
   return (
-    <div>PurchaseItemEntry</div>
-  )
+        <div className="tankEntries">
+            <DivisionTopBar sectionTitle="Tank entry information">
+                <AddItemButton btnCaption="Add Tank Entry"/>
+            </DivisionTopBar>
+            <div>
+                <FuelPurchaseEntry />
+            </div>
+        </div>
+    )
 }
 
 export default PurchaseItemEntry

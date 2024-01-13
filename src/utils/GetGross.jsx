ba92ba759547e6cd31fe09583030c3d0e4e8b32e
paullaster <paullasterokoth98@gmail.com>
2024-01-13 08:22:09 +0300

    update: update item ui

--- src/utils/GetGross.jsx
+++ src/utils/GetGross.jsx
@@ -1,7 +1,9 @@
 const GetGross = (item) => {
-  return (
-    (Number(item.quantity)  || 0) * (Number(item.price) || 0)) + ((Number(item.vat_rate || 0) / 100) * (Number(item.quantity)  || 0) * (Number(item.price) || 0)
-  )
+    console.log(item);
+    const vatRate = item.vat_rate.split(',').toSpliced()
+    const amount = (Number(item.quantity)  || 0) * (Number(item.price) || 0);
+    const tax_amount = (Number(item.vat_rate || 0) / 100) * (Number(item.quantity) || 0);
+  return amount + tax_amount;
 }
 
 export default GetGross

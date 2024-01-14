827453de5f80f5d2490bae2e8c028ba77fe82840
paullaster <paullasterokoth98@gmail.com>
2024-01-14 16:06:26 +0300

    update the purchase section

--- src/components/shared/index.jsx
+++ src/components/shared/index.jsx
@@ -10,3 +10,4 @@ export { default as Stepper} from './Stepper';
 export { default as DatePickerComponent } from './DatePickerComponent';
 export { default as DataTable } from './DataTable';
 export { default as AutocompleteComponent } from './Autocomplete';
+export { default as DialogComponent } from './DialogComponent';

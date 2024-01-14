c37bfdf520c4eb4d06dbfd3139ceadba5f542c47
paullaster <paullasterokoth98@gmail.com>
2024-01-14 18:37:21 +0300

    update the purchase section

--- src/components/shared/DialogComponent.jsx
+++ src/components/shared/DialogComponent.jsx
@@ -5,15 +5,12 @@ import DialogContentText from '@mui/material/DialogContentText';
 import DialogActions from '@mui/material/DialogActions';
 import Button from '@mui/material/Button';
 
-const DialogComponent = ({open, handleClose, maxWidth = 'sm', fullWidth= true,  dialogTitle = '', dialogContentText = '', cancelLabel = 'Close', dialogActionButtonLabelType = 'submit', dialogActionButtonLabel = 'Save', children}) => {
+const DialogComponent = ({open, handleClose,  dialogTitle = '', dialogContentText = '', cancelLabel = 'Close', dialogActionButtonLabelType = 'submit', dialogActionButtonLabel = 'Save', children}) => {
   return (
     <Dialog
     open={open}
     onClose={handleClose}
     PaperProps={{}}
-    maxWidth={maxWidth}
-    fullWidth={fullWidth}
-
     >
       <DialogTitle>{dialogTitle}</DialogTitle>
       <DialogContent>

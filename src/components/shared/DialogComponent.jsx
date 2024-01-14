ab1086c66ec7f2d8dae53c6d951b8cfee07d7cd5
paullaster <paullasterokoth98@gmail.com>
2024-01-14 16:38:23 +0300

    update the purchase section

--- src/components/shared/DialogComponent.jsx
+++ src/components/shared/DialogComponent.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogActions from '@mui/material/DialogActions';
 
-const DialogComponent = () => {
+const DialogComponent = ({dialogTitle = '', dialogContentText = '', }) => {
+  const [open, setOpen] = useState(false);
+
+  const handleClose = () => {
+    setOpen(false);
+  }
   return (
-    <div>DialogComponent</div>
+    <Dialog
+    open={open}
+    onClose={handleClose}
+    PaperProps={{}}
+
+    >
+      <DialogTitle>{dialogTitle}</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          {dialogContentText}
+        </DialogContentText>
+
+      </DialogContent>
+
+    </Dialog>
   )
 }
 

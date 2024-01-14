67eac53bb98f67d11b8ec57e1196501622a8953e
paullaster <paullasterokoth98@gmail.com>
2024-01-14 10:54:02 +0300

    update the purchase section

--- src/components/shared/Autocomplete.jsx
+++ src/components/shared/Autocomplete.jsx
@@ -1,6 +1,6 @@
 import { ComboBox, Label } from "react-aria-components";
-import CustomPopover from "./CustomPopover";
-import CustomAriaInput from "./CustomAriaInput";
+import CustomPopover from "../../packages/purchases/shared/components/CustomPopover";
+import CustomAriaInput from "../../packages/purchases/shared/components/CustomAriaInput";
 
 const label = " ";
 const Autocomplete = ({list, label}) => {

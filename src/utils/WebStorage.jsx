7dfd66cbd4924beb4150f539e6c993f8f9e70053
paullaster <paullasterokoth98@gmail.com>
2024-01-16 00:42:52 +0300

    update purchase ui

--- src/utils/WebStorage.jsx
+++ src/utils/WebStorage.jsx
@@ -5,7 +5,22 @@ class WebStorage {
             case 'local':
                 localStorage.setItem(resourceName, JSON.stringify(resource));
                 break;
-            case
+            case 'session':
+                sessionStorage.setItem(resourceName, JSON.stringify(resource));
+                break;
+            case 'cookie':
+                const cookies = document.cookie;
+                
+        }
+    }
+    CheckItemIfExist(tray, item, typeOfTray, keyInTrayItem =  '') {
+        switch(typeOfTray) {
+            case 'array':
+                const obj = tray.find((i) => {
+                    if (keyInTrayItem !== '') {
+                        return i[keyInTrayItem] === item;
+                    }
+                })
         }
     }
 

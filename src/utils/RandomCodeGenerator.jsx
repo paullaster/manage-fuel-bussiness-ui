051f4aa731a0ce976f3f3e6000de4baa6dbad3b1
paullaster <paullasterokoth98@gmail.com>
2024-01-15 06:21:16 +0300

    development: adding unique code for puchase record

--- src/utils/RandomCodeGenerator.jsx
+++ src/utils/RandomCodeGenerator.jsx
@@ -2,8 +2,15 @@ const alphabet = 'abcdefghijklmnopqrstuvwxyz';
 const digits = '0123456789';
 
 const RandomCodeGenerator = () => {
+    const generatedCodes = new Set();
+    const prefix = 'aaa';
     const alphabetArray = alphabet.toUpperCase().split('');
     const digitArray = digits.split('');
     const incrementalCode = '';
+
+
+    while(true) {
+        
+    }
 
 }

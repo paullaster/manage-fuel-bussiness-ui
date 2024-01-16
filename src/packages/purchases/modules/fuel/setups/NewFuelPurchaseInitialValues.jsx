9bc84df2904706e0e4420be0c19018687e2a4ce9
paullaster <paullasterokoth98@gmail.com>
2024-01-16 05:29:39 +0300

    impovement: improve purchase ui

--- src/packages/purchases/modules/fuel/setups/NewFuelPurchaseInitialValues.jsx
+++ src/packages/purchases/modules/fuel/setups/NewFuelPurchaseInitialValues.jsx
@@ -1,8 +1,8 @@
 export default {
     dip_quantity_before_offloading: "0",
     sales_quantity_during_offloading: "0",
-    expected_quantity: "string",
-    actual_dip_quantity_after_offloading: "string",
+    expected_quantity: "0",
+    actual_dip_quantity_after_offloading: "0",
     tank: 'select tank',
     variance: "string",
     price: "string",

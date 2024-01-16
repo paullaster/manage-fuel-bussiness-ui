78bf68698dc9ce9012a72856462a379aec3791df
paullaster <paullasterokoth98@gmail.com>
2024-01-16 05:20:23 +0300

    impovement: improve purchase ui

--- src/packages/purchases/modules/items/components/NewItem.jsx
+++ src/packages/purchases/modules/items/components/NewItem.jsx
@@ -43,7 +43,7 @@ const NewItem = () => {
     event.stopPropagation();
     event.preventDefault();
     setTableDataRows((prev) => prev.filter((line) => line.id !== item.id));
-  }
+  };
 
 
   useEffect(() => {

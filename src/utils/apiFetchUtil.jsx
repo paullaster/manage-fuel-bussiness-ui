accc6df62bafcb2495baa4c645e8f66122a8cf8b
paullaster <paullasterokoth98@gmail.com>
2024-01-16 06:57:32 +0300

    impovement: improve purchase ui

--- src/utils/apiFetchUtil.jsx
+++ src/utils/apiFetchUtil.jsx
@@ -1,7 +1,9 @@
+import { _request } from '@/services';
+
 export const apiFetchUtil = async (options = [], type = '') => {
   console.log(options)
   switch(type) {
     case 'fuel_type':
-        
+
   }
 };

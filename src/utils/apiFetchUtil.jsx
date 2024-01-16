3446873c938ea1269f26019d688a51d74b3e9399
paullaster <paullasterokoth98@gmail.com>
2024-01-16 07:00:17 +0300

    impovement: improve purchase ui

--- src/utils/apiFetchUtil.jsx
+++ src/utils/apiFetchUtil.jsx
@@ -2,8 +2,15 @@ import { _request } from '@/services';
 
 export const apiFetchUtil = async (options = [], type = '') => {
   console.log(options)
+  let response = '';
   switch(type) {
     case 'fuel_type':
-
+        response = await _request({
+            method: 'GET',
+            params: {
+                fuel_type_id: options.values,
+            },
+            url: 
+        })
   }
 };

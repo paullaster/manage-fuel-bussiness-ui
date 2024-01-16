3dc20ba1d4f656771d6445c55a8dbfb3ac8db6f3
paullaster <paullasterokoth98@gmail.com>
2024-01-16 05:46:56 +0300

    impovement: improve purchase ui

--- src/utils/GetGross.jsx
+++ src/utils/GetGross.jsx
@@ -1,5 +1,5 @@
-const GetGross = (item) => {
-    let vatRate = item.vat_rate.split('');
+const GetGross = (item, field) => {
+    let vatRate = item[field].split('');
     vatRate = vatRate.toSpliced(vatRate.indexOf('%'), 1).join('');
     const amount = (Number(item.quantity)  || 0) * (Number(item.price) || 0);
     const tax_amount = ((Number(vatRate || 0) / 100) * (Number(item.price) || 0)) * (Number(item.quantity)  || 0);

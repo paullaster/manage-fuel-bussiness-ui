6134e04e16358614061bdb53653bb1e7b5af9226
paullaster <paullasterokoth98@gmail.com>
2024-01-16 05:25:12 +0300

    impovement: improve purchase ui

---
+++ src/packages/purchases/modules/fuel/setups/NewFuelPurchaseInitialValues.jsx
@@ -0,0 +1,21 @@
+export default {
+    "tank_entries": [
+      {
+        "dip_quantity_before_offloading": "string",
+        "sales_quantity_during_offloading": "string",
+        "expected_quantity": "string",
+        "actual_dip_quantity_after_offloading": "string",
+        "tank": 0,
+        "variance": "string"
+      }
+    ],
+    "quantity": "string",
+    "price": "string",
+    "gross_amount": "string",
+    "tax_rate": "string",
+    "tax_amount": "string",
+    "net_payable": "string",
+    "officer": 0,
+    "vendor": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+    "product": 0
+  }

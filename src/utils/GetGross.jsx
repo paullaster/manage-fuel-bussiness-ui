e3e4faf23fc8198fb1186ded56cc45e2406fe384
paullaster <paullasterokoth98@gmail.com>
2024-01-16 21:20:28 +0300

    refactor: refactor gret gross util

--- src/utils/GetGross.jsx
+++ src/utils/GetGross.jsx
@@ -1,15 +1,16 @@
-const GetGross = (item, taxField, quantityField, priceField, type ) => {
-    let vatRate = item[taxField].split('');
-    vatRate = vatRate.toSpliced(vatRate.indexOf('%'), 1).join('');
-    const tax_amount = ((Number(vatRate || 0) / 100) * (Number(item[priceField]) || 0)) * (Number(item[quantityField])  || 0);
-    
-    switch(type) {
-      case 'tax_amount':
-        return tax_amount;
-        case 'gross_amount':
-          const amount = (Number(item[quantityField])  || 0) * (Number(item[priceField]) || 0);
-          return amount + tax_amount || 0 ;
-    }
+const GetGross = (item, taxField, quantityField, priceField, type) => {
+  let vatRate = item[taxField].split('');
+  vatRate = vatRate.toSpliced(vatRate.indexOf('%'), 1).join('');
+  const tax_amount = ((Number(vatRate || 0) / 100) * (Number(item[priceField]) || 0)) * (Number(item[quantityField]) || 0);
+  let amount = 0;
+  switch (type) {
+    case 'tax_amount':
+      return tax_amount;
+    case 'gross_amount':
+      amount = (Number(item[quantityField]) || 0) * (Number(item[priceField]) || 0);
+      return amount + tax_amount || 0;
+    default: return new Error('Undefined type');
+  }
 }
 
 export default GetGross

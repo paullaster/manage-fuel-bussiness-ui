81ca3fc0c18636077bf814dfd592edc9b3a35592
paullaster <paullasterokoth98@gmail.com>
2024-01-17 06:12:00 +0300

    update delete action of table items

--- src/packages/purchases/modules/fuel/components/NewFuelPurchase.jsx
+++ src/packages/purchases/modules/fuel/components/NewFuelPurchase.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useMemo } from "react";
+import { useEffect, useState, useMemo, useCallback } from "react";
 import shared from "../../../shared";
 import { useGlobalDispatcher, useGlobalState } from '@/store';
 import { Form } from "react-router-dom";
@@ -9,40 +9,143 @@ import FormButtonRow from "../../../shared/components/FormButtonRow";
 import NewVendor from "../../vendors/components/NewVendor";
 import { v4 as uuidv4 } from 'uuid';
 import { ArrayFunctions } from "@/utils";
+import { GridActionsCellItem } from '@mui/x-data-grid';
+import { MdDelete } from "react-icons/md";
+import DeleteIcon from '@mui/icons-material/Delete';
+import SecurityIcon from '@mui/icons-material/Security';
+import FileCopyIcon from '@mui/icons-material/FileCopy';
 
 
 
 const NewFuelPurchase = () => {
-  const [tableDataRows, setTableDataRows] = useState([]);
+  const [rows, setRows] = useState([]);
   const appStateDispatcher = useGlobalDispatcher();
   const { cardLabelView } = useGlobalState();
+  // const handleDeletingLineItem = useCallback((item) => {
+  //   setTableDataRows((prev) => prev.filter((line) => line.id !== item.id));
+  // }, []);
 
 
-  const columns = useMemo(() => {
-    return ArrayFunctions({
-      arrKey: 'field',
-      itemKey: 'field',
-      item: { field: 'action' },
-      propToUpdate: 'renderCell',
-      op: 'equal',
-      update: (params) => <shared.components.FormAction row={params} onDelete={handleDeletingLineItem} />,
-      type: 'map',
-    }, purchaseEntryColumns);
+  // const columns = useMemo(() => {
+  //   return ArrayFunctions({
+  //     arrKey: 'field',
+  //     itemKey: 'field',
+  //     item: { field: 'action' },
+  //     propToUpdate: 'getActions',
+  //     op: 'equal',
+  //     update: (params) => 
+  //       <GridActionsCellItem 
+  //        icon={<MdDelete />}
+  //        label="Delete"
+  //        onClick={handleDeletingLineItem(params)}
+  //       />,
+  //     type: 'map',
+  //   }, purchaseEntryColumns);
 
-  }, []);
+  // }, []);
+  const deleteUser = useCallback(
+    (id) => () => {
+      setTimeout(() => {
+        setRows((prevRows) => prevRows.filter((row) => row.id !== id));
+      });
+    },
+    [],
+  );
 
+  const toggleAdmin = useCallback(
+    (id) => () => {
+      setRows((prevRows) =>
+        prevRows.map((row) =>
+          row.id === id ? { ...row, isAdmin: !row.isAdmin } : row,
+        ),
+      );
+    },
+    [],
+  );
+
+  const duplicateUser = useCallback(
+    (id) => () => {
+      setRows((prevRows) => {
+        const rowToDuplicate = prevRows.find((row) => row.id === id);
+        return [...prevRows, { ...rowToDuplicate, id: Date.now() }];
+      });
+    },
+    [],
+  );
+  const columns = useMemo(
+    () => [
+      { field: 'name', type: 'string' },
+      { field: 'age', type: 'number' },
+      { field: 'dateCreated', type: 'date', width: 130 },
+      { field: 'lastLogin', type: 'dateTime', width: 180 },
+      { field: 'isAdmin', type: 'boolean', width: 120 },
+      {
+        field: 'country',
+        type: 'singleSelect',
+        width: 120,
+        valueOptions: [
+          'Bulgaria',
+          'Netherlands',
+          'France',
+          'United Kingdom',
+          'Spain',
+          'Brazil',
+        ],
+      },
+      {
+        field: 'discount',
+        type: 'singleSelect',
+        width: 120,
+        editable: true,
+        valueOptions: ({ row }) => {
+          if (row === undefined) {
+            return ['EU-resident', 'junior'];
+          }
+          const options = [];
+          if (!['United Kingdom', 'Brazil'].includes(row.country)) {
+            options.push('EU-resident');
+          }
+          if (row.age < 27) {
+            options.push('junior');
+          }
+          return options;
+        },
+      },
+      {
+        field: 'actions',
+        type: 'actions',
+        width: 80,
+        getActions: (params) => [
+          <GridActionsCellItem
+            icon={<DeleteIcon />}
+            label="Delete"
+            onClick={deleteUser(params.id)}
+          />,
+          <GridActionsCellItem
+            icon={<SecurityIcon />}
+            label="Toggle Admin"
+            onClick={toggleAdmin(params.id)}
+            showInMenu
+          />,
+          <GridActionsCellItem
+            icon={<FileCopyIcon />}
+            label="Duplicate User"
+            onClick={duplicateUser(params.id)}
+            showInMenu
+          />,
+        ],
+      },
+    ],
+    [deleteUser, toggleAdmin, duplicateUser],
+  );
+
+  console.log(columns);
   const handleAddNewItem = (event) => {
     event.preventDefault();
     event.stopPropagation();
-    setTableDataRows((prev) => [...prev, { id: uuidv4(), ...NewFuelPurchaseInitialValues }]);
+    setRows((prev) => [...prev, { id: uuidv4(), ...NewFuelPurchaseInitialValues }]);
   };
 
-  const handleDeletingLineItem = (event, item) => {
-    console.log(item)
-    event.stopPropagation();
-    event.preventDefault();
-    setTableDataRows((prev) => prev.filter((line) => line.id !== item.id));
-  };
 
   useEffect(() => {
     appStateDispatcher({ type: "CREATECOMPOSABLEAUTOFILS", payload: composableAutofils });
@@ -56,7 +159,7 @@ const NewFuelPurchase = () => {
           <NewVendor />
         </shared.components.BillingComponent>
         <TransportationAndOfficer cardLabelView={cardLabelView} />
-        <TankEntries columns={columns} rows={tableDataRows} handleAddNewItem={handleAddNewItem} />
+        <TankEntries columns={columns} rows={rows} handleAddNewItem={handleAddNewItem} />
         <FormButtonRow />
       </Form>
     </section>

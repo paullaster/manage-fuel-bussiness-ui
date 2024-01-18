a4ef8918044e8c71f8954fbbcbe4cb100d42d640
paullaster <paullasterokoth98@gmail.com>
2024-01-18 06:35:08 +0300

    update purchases ui

--- src/utils/GetGross.jsx
+++ src/utils/GetGross.jsx
@@ -1,9 +1,5 @@
 const GetGross = (item, taxField, quantityField, priceField, type) => {
-  if (typeof taxField !== 'string') {
-    taxField = taxField.toString();
-  }
-  let vatRate = item[taxField].split('');
-  vatRate = vatRate.toSpliced(vatRate.indexOf('%'), 1).join('');
+  vatRate = item[taxField]
   const tax_amount = ((Number(vatRate || 0) / 100) * (Number(item[priceField]) || 0)) * (Number(item[quantityField]) || 0);
   let amount = 0;
   switch (type) {

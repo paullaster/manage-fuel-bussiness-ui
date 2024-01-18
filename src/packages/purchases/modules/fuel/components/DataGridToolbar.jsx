9553f17da8aead26f8b6e394102a7b1e9501385b
paullaster <paullasterokoth98@gmail.com>
2024-01-18 05:35:20 +0300

    update purchases ui

---
+++ src/packages/purchases/modules/fuel/components/DataGridToolbar.jsx
@@ -0,0 +1,14 @@
+import { 
+    GridActionsCellItem,
+    GridRowModes,
+    GridToolbarContainer,
+    GridRowEditStopReasons
+   } from '@mui/x-data-grid';
+
+const DataGridToolbar = () => {
+  return (
+    <div>DataGridToolbar</div>
+  )
+}
+
+export default DataGridToolbar

ffe584fd93ea90a85281fd65c280ec165f618631
paullaster <paullasterokoth98@gmail.com>
2024-01-18 05:44:08 +0300

    update purchases ui

--- src/components/shared/DataGridToolbar.jsx
+++ src/components/shared/DataGridToolbar.jsx
@@ -1,14 +1,14 @@
-import { 
-    GridActionsCellItem,
-    GridRowModes,
-    GridToolbarContainer,
-    GridRowEditStopReasons
-   } from '@mui/x-data-grid';
-   import Button from '@mui/material/Button';
-const DataGridToolbar = ({methodHandler}) => {
+import {
+  GridActionsCellItem,
+  GridRowModes,
+  GridToolbarContainer,
+  GridRowEditStopReasons
+} from '@mui/x-data-grid';
+import Button from '@mui/material/Button';
+const DataGridToolbar = ({ methodHandler }) => {
   return (
     <GridToolbarContainer>
-        <Button color="primary" startIcon={<AddIcon />} onClick={handleClick}>
+      <Button color="primary" startIcon={<AddIcon />} onClick={methodHandler}>
         Add record
       </Button>
     </GridToolbarContainer>

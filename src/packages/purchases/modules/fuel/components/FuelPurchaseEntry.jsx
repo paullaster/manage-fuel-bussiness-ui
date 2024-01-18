3a4846cdfc5080b104cff11dc59d7be697726e3c
paullaster <paullasterokoth98@gmail.com>
2024-01-18 06:11:58 +0300

    update purchases ui

--- src/packages/purchases/modules/fuel/components/FuelPurchaseEntry.jsx
+++ src/packages/purchases/modules/fuel/components/FuelPurchaseEntry.jsx
@@ -1,10 +1,11 @@
 import { DataTable } from "@/components";
-const FuelPurchaseEntry = ({columns, rows}) => {
+const FuelPurchaseEntry = ({columns, rows, slots = {}, slotProps = {}}) => {
   return (
     <DataTable
       columns={columns}
       rows={rows}
-
+      slots={slots}
+      slotProps={slotProps}
     />
   )
 }

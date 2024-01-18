89c88410583c778b1feecb1c3badf8293b0fcfa8
paullaster <paullasterokoth98@gmail.com>
2024-01-18 07:04:18 +0300

    update purchases ui

--- src/packages/purchases/modules/fuel/components/TankEntries.jsx
+++ src/packages/purchases/modules/fuel/components/TankEntries.jsx
@@ -1,14 +1,19 @@
 
 import FuelPurchaseEntry from "./FuelPurchaseEntry"
-import AddItemButton from "../../../shared/components/AddItemButton"
 import DivisionTopBar from "../../../shared/components/DivisionTopBar"
-const TankEntries = ({columns, rows, slots = {}, slotProps = {}}) => {
+const TankEntries = ({columns, rows, rowModesModel = {}, slots = {}, slotProps = {}}) => {
     return (
         <div className="tankEntries">
             <DivisionTopBar sectionTitle="Tank entry information">
             </DivisionTopBar>
             <div>
-                <FuelPurchaseEntry columns={columns} rows ={rows} slots = {slots} slotProps = {slotProps}/>
+                <FuelPurchaseEntry 
+                columns={columns} 
+                rows ={rows} 
+                rowModesModel={rowModesModel}
+                slots = {slots} 
+                slotProps = {slotProps}
+                />
             </div>
         </div>
     )

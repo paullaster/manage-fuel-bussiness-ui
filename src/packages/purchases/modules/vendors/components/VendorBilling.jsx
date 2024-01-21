d48922cab71a570f96f93199d16516502e701c59
paullaster <paullasterokoth98@gmail.com>
2024-01-21 22:41:18 +0300

    update purchase

--- src/packages/purchases/modules/vendors/components/VendorBilling.jsx
+++ src/packages/purchases/modules/vendors/components/VendorBilling.jsx
@@ -2,43 +2,40 @@ import React, { useState } from 'react'
 import { AutocompleteComponent, InputComponent } from '@/components';
 
 const VendorBilling = () => {
-    const [paymentMethods, setPaymentMethods] = useState([{method: 'MPESA'}]);
+    const [paymentMethods, setPaymentMethods] = useState([{ method: 'MPESA' }]);
     return (
         <div className="billinginfo_dataentry">
-            <div className="new_vendors__left__dataentry__form_billinginfo_dataentry_section-one">
-            <AutocompleteComponent 
-            list={paymentMethods}
-            label={'Payment methods'}
-            keyField={'method'}
+            <AutocompleteComponent
+                list={paymentMethods}
+                label={'Payment methods'}
+                keyField={'method'}
             />
-                <InputComponent
-                    type="text"
-                    prelabelText={"Phone"}
-                    name="mpesa_phone_number"
-                    title="Mpesa phone number"
-                />
-                <InputComponent
-                    type="text"
-                    prelabelText={"Mpesa Till number"}
-                    name="mpesa_till_number"
-                />
-                <InputComponent
-                    type="text"
-                    prelabelText={"Mpesa Paybill number"}
-                    name="mpesa_paybill_number"
-                />
-                <InputComponent
-                    type="text"
-                    prelabelText={"Bank name"}
-                    name="bank_name"
-                />
-                <InputComponent
-                    type="text"
-                    prelabelText={"Account number"}
-                    name="bank_account_number"
-                />
-            </div>
-
+            <InputComponent
+                type="text"
+                prelabelText={"Phone"}
+                name="mpesa_phone_number"
+                title="Mpesa phone number"
+            />
+            <InputComponent
+                type="text"
+                prelabelText={"Mpesa Till number"}
+                name="mpesa_till_number"
+            />
+            <InputComponent
+                type="text"
+                prelabelText={"Mpesa Paybill number"}
+                name="mpesa_paybill_number"
+            />
+            <InputComponent
+                type="text"
+                prelabelText={"Bank name"}
+                name="bank_name"
+            />
+            <InputComponent
+                type="text"
+                prelabelText={"Account number"}
+                name="bank_account_number"
+            />
         </div>
     )
 }

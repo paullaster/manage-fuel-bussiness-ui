f8e03c717eff390faa37a51da37bd496077e019c
paullaster <paullasterokoth98@gmail.com>
2024-01-22 05:28:28 +0300

    postinf new vendor

--- src/packages/purchases/modules/vendors/components/ContactPerson.jsx
+++ src/packages/purchases/modules/vendors/components/ContactPerson.jsx
@@ -3,6 +3,7 @@ import { DataTable } from '@/components';
 import { GridToolbarContainer, GridRowModes, GridActionsCellItem } from '@mui/x-data-grid';
 import Button from '@mui/material/Button';
 import { v4 as uuidv4 } from 'uuid';
+import { MdOutlineSaveAlt } from 'react-icons/md';
 
 const rowInitialValues = {
     contact_name: '',
@@ -10,16 +11,16 @@ const rowInitialValues = {
     contact_phone_number: '',
 };
 
-const GridTableToolbar = ({setRows, setRowModesModel}) => {
+const GridTableToolbar = ({ setRows, setRowModesModel }) => {
     const handleAddcontact = () => {
         const id = uuidv4();
         setRows(
-            (rows) => [...rows, {id, ...rowInitialValues, isNew: true}]
+            (rows) => [...rows, { id, ...rowInitialValues, isNew: true }]
         );
         setRowModesModel(
-            (rowModesModel) => ({ 
+            (rowModesModel) => ({
                 ...rowModesModel,
-                [id]: { mode: GridRowModes.Edit, fieldToFocus: 'contact_name'}
+                [id]: { mode: GridRowModes.Edit, fieldToFocus: 'contact_name' }
             })
         );
     }
@@ -36,96 +37,93 @@ const GridTableToolbar = ({setRows, setRowModesModel}) => {
 
 
 const ContactPerson = () => {
-  const [rows, setRows] = useState([]);
-  const [rowModesModel, setRowModesModel] = useState({});
+    const [rows, setRows] = useState([]);
+    const [rowModesModel, setRowModesModel] = useState({});
 
 
 
-  const contactColumns = useMemo(() => {
-    return [
-        {
-            field: 'contact_name',
-            headerName: 'Name',
-            type: 'string',
-            width: 250,
-            headerAlign: 'center',
-            editable: true,
-        },
-        {
-            field: 'contact_email',
-            headerName: 'Email',
-            type: 'string',
-            width: 250,
-            headerAlign: 'center',
-            editable: true,
-        },
-        {
-            field: 'contact_phone_number',
-            headerName: 'Phone',
-            type: 'string',
-            width: 250,
-            headerAlign: 'center',
-            editable: true
-        },
-        {
-            field: 'actions',
-            headerName: 'Action',
-            type: 'actions',
-            getActions: (params) => {
-                const isInEditMode = rowModesModel[params.id]?.mode === GridRowModes.Edit;
+    const contactColumns = useMemo(() => {
+        return [
+            {
+                field: 'contact_name',
+                headerName: 'Name',
+                type: 'string',
+                width: 250,
+                headerAlign: 'center',
+                editable: true,
+            },
+            {
+                field: 'contact_email',
+                headerName: 'Email',
+                type: 'string',
+                width: 250,
+                headerAlign: 'center',
+                editable: true,
+            },
+            {
+                field: 'contact_phone_number',
+                headerName: 'Phone',
+                type: 'string',
+                width: 250,
+                headerAlign: 'center',
+                editable: true
+            },
+            {
+                field: 'actions',
+                headerName: 'Action',
+                type: 'actions',
+                getActions: (params) => {
+                    const isInEditMode = rowModesModel[params.id]?.mode === GridRowModes.Edit;
 
-          if (isInEditMode) {
-            return [
-              <GridActionsCellItem 
-              key={uuidv4()}
-              icon={<MdOutlineSaveAlt />}
-              label="Save"
-              sx={{
-                color: 'primary.main',
-              }}
-              onClick={handleSaveClick(params.id)}
-              />,
-              <GridActionsCellItem
-              key={uuidv4()}
-              icon={<MdCancel />}
-              label="Cancel"
-              className="textPrimary"
-              onClick={handleCancelClick(params.id)}
-              color="inherit"
-            />,
-            ];
-          }
-          return[
-            <GridActionsCellItem
-              key={uuidv4()}
-              icon={<MdCreate size={25} />}
-              label="Edit"
-              onClick={handleEditClick(params.id)}
-            />,
-            <GridActionsCellItem
-              key={uuidv4()}
-              icon={<MdDelete size={25} />}
-              label="Delete"
-              onClick={deleteItem(params.id)}
-            />,
-          ]
+                    if (isInEditMode) {
+                        return [
+                            <GridActionsCellItem
+                                key={uuidv4()}
+                                icon={<MdOutlineSaveAlt />}
+                                label="Save"
+                                onClick={handleSaveClick(params.id)}
+                            />,
+                            <GridActionsCellItem
+                                key={uuidv4()}
+                                icon={<MdCancel />}
+                                label="Cancel"
+                                className="textPrimary"
+                                onClick={handleCancelClick(params.id)}
+                                color="inherit"
+                            />,
+                        ];
+                    }
+                    return [
+                        <GridActionsCellItem
+                            key={uuidv4()}
+                            icon={<MdCreate size={25} />}
+                            label="Edit"
+                            onClick={handleEditClick(params.id)}
+                        />,
+                        <GridActionsCellItem
+                            key={uuidv4()}
+                            icon={<MdDelete size={25} />}
+                            label="Delete"
+                            onClick={deleteItem(params.id)}
+                        />,
+                    ]
+                }
             }
-        }
-    ]
-  })  
-  return (
-    <DataTable 
-     columns={contactColumns}
-     rows={rows}
-     rowModesModel={rowModesModel}
-     slots={
-        {toolbar: GridTableToolbar}
-     }
-     slotProps={{
-        toolbar: {setRows, setRowModesModel}
-     }}
-    />
-  )
+        ]
+    })
+    return (
+        <DataTable
+            columns={contactColumns}
+            rows={rows}
+            rowModesModel={rowModesModel}
+            slots={
+                { toolbar: GridTableToolbar }
+            }
+            slotProps={{
+                toolbar: { setRows, setRowModesModel }
+            }}
+        />
+    )
 }
 
 export default ContactPerson

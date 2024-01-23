d7d0ea1a7ecb8f81fce27fc58070c483f8a28056
paullaster <paullasterokoth98@gmail.com>
2024-01-23 05:23:42 +0300

    updated purchase posting

--- src/packages/purchases/shared/components/AddItemButton.jsx
+++ src/packages/purchases/shared/components/AddItemButton.jsx
@@ -1,10 +1,10 @@
 import { MdAdd } from "react-icons/md";
 import { Button, } from "@/components";
 
-const AddItemButton = ({methodHandler = () =>console.log("create new item"), btnCaption = "caption"}) => {
+const AddItemButton = ({methodHandler = () =>console.log("create new item"), btnCaption = "caption", btnCustomClass = {}}) => {
     return (
         <div className="button_container">
-            <Button onClick={methodHandler}>
+            <Button onClick={methodHandler} className={btnCustomClass}>
                 <span><MdAdd size={20} /></span>
                 <span>{btnCaption}</span>
             </Button>

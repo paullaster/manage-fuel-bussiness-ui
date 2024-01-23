acdf75d385b082ddf19c0eee1cd4adf802759289
paullaster <paullasterokoth98@gmail.com>
2024-01-23 05:36:35 +0300

    updated purchase posting

--- src/packages/purchases/modules/vendors/components/CurrencyComponent.jsx
+++ src/packages/purchases/modules/vendors/components/CurrencyComponent.jsx
@@ -1,6 +1,8 @@
 import { DialogComponent, InputComponent } from '@/components';
+import { forwardRef } from 'react';
 
-const CurrencyComponent = () => {
+const CurrencyComponent = forwardRef((props, ref) => {
+    const { open,  handleCloseDialog, dialogTitle, maxWidth } = props;
     return (
         <DialogComponent
             open={open}
@@ -8,12 +10,34 @@ const CurrencyComponent = () => {
             dialogTitle={dialogTitle}
             maxWidth={maxWidth}
         >
-            <InputComponent 
-            
+             <InputComponent
+                type="text"
+                prelabelText={"Currency name"}
+                name="currency_name"
+                title="currency name"
+                ref={ref.currencyNameRef}
+            />
+            <InputComponent
+                type="text"
+                prelabelText={"Currency code"}
+                name="currency_code"
+                ref={ref.currencyCodeRef}
+            />
+            <InputComponent
+                type="text"
+                prelabelText={"Rate"}
+                name="rate"
+                ref={ref.currencyRateRef}
+            />
+            <InputComponent
+                type="text"
+                prelabelText={"Symbol"}
+                name="symbol"
+                ref={ref.currencySymbolref}
             />
 
         </DialogComponent>
     )
-}
+})
 
 export default CurrencyComponent

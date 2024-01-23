23adafebe7f4d97c924d3c3872fa3305ad4e2a96
paullaster <paullasterokoth98@gmail.com>
2024-01-24 00:56:38 +0300

    update purchase posting

--- src/packages/purchases/modules/items/components/OfficerComponent.jsx
+++ src/packages/purchases/modules/items/components/OfficerComponent.jsx
@@ -2,19 +2,19 @@ import { AutocompleteComponent } from '@/components';
 import DivisionTopBar from "../../../shared/components/DivisionTopBar";
 import { useState } from 'react';
 
-const OfficerComponent = ({handleSelectedOficer}) => {
-  const [officers, setOfficers] = useState([{name: 'Ken Mjungu'}, {name: 'Waigah Mwaura'}]);
-  return (
-    <div>
-        <DivisionTopBar sectionTitle="Officer details" />
-        <AutocompleteComponent
+const OfficerComponent = ({ handleSelectedOficer }) => {
+    const [officers, setOfficers] = useState([{ name: 'Ken Mjungu' }, { name: 'Waigah Mwaura' }]);
+    return (
+        <div className='itemOfficer'>
+            <DivisionTopBar sectionTitle="Officer details" />
+            <AutocompleteComponent
                 list={officers}
                 label={'Select Officer'}
                 keyField={'name'}
                 handleOnchange={handleSelectedOficer}
             />
-    </div>
-  )
+        </div>
+    )
 }
 
 export default OfficerComponent

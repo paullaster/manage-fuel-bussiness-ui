418f140e80dedc15d688839fec9957fb950f2c94
paullaster <paullasterokoth98@gmail.com>
2024-01-23 23:54:30 +0300

    update purchase posting

--- src/packages/purchases/modules/items/components/NewItem.jsx
+++ src/packages/purchases/modules/items/components/NewItem.jsx
@@ -194,9 +194,9 @@ setRowModesModel(newRowModesModel);
     [handleSaveClick, handleCancelClick, handleEditClick,  deleteItem],
   );
 
-  useEffect(() => {
-    appStateDispatcher({ type: "CREATECOMPOSABLEAUTOFILS", payload: composableAutofils });
-  }, [columns]);
+  // useEffect(() => {
+  //   appStateDispatcher({ type: "CREATECOMPOSABLEAUTOFILS", payload: composableAutofils });
+  // }, [columns]);
 
   return (
     <section className='purchaseItem'>

d9bf4db629a8318116ecc6811f9aa8d3253f1b70
paullaster <paullasterokoth98@gmail.com>
2024-01-24 00:53:14 +0300

    update purchase posting

--- src/packages/purchases/modules/items/components/OfficerComponent.jsx
+++ src/packages/purchases/modules/items/components/OfficerComponent.jsx
@@ -3,12 +3,12 @@ import DivisionTopBar from "../../../shared/components/DivisionTopBar";
 import { useState } from 'react';
 
 const OfficerComponent = () => {
-  const [officer, setOfficer] = useState([{name: 'Ken Mjungu'}, {name: 'Waigah Mwaura'}]);
+  const [officers, setOfficers] = useState([{name: 'Ken Mjungu'}, {name: 'Waigah Mwaura'}]);
   return (
     <div>
         <DivisionTopBar sectionTitle="Purcahse item details" />
         <AutocompleteComponent
-                list={paymentMethods}
+                list={officers}
                 label={'Select Officer'}
                 keyField={'name'}
                 handleOnchange={handleSelectedPaymentMethod}

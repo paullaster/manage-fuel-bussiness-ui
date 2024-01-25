b180b65ade882a9acd4ea970c48e98fbc0f60aea
paullaster <paullasterokoth98@gmail.com>
2024-01-25 05:59:16 +0300

    update vendor

---
+++ src/utils/ObjectValidator.jsx
@@ -0,0 +1,18 @@
+const ObjectValidator = (requiredFields, Obj) => {
+    const requiredFieldsSet = new Set([
+        ...requiredFields
+    ]);
+
+    const missingRequiredFields = [];
+
+    for (let prop in Obj) {
+        if (requiredFields.has(prop) && !Obj[prop]) {
+            missingRequiredFields.push(prop);
+        }
+    }
+    if (missingRequiredFields.length) {
+        throw new Error(`The following required fields are missing: ${missingFields.join(", ")}`);
+    }
+    return true;
+
+}

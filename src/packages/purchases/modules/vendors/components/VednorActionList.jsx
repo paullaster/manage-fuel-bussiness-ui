8df21af9310127ef51c408b8fbc266cdd125811b
paullaster <paullasterokoth98@gmail.com>
2024-01-28 01:46:42 +0300

    refactor: refactor ui

--- src/packages/purchases/modules/vendors/components/VednorActionList.jsx
+++ src/packages/purchases/modules/vendors/components/VednorActionList.jsx
@@ -4,6 +4,9 @@ export default
     caption: 'New',
     iconList: [],
     hasList: true,
-    list: [ {cap: 'Fuel purchase', action: () => {console.log("Fuel purchase")}}]
+    list: [ 
+        {cap: 'Fuel purchase', action: () => {console.log("Fuel purchase")}},
+        {cap: 'Item purchase', action: () => {console.log("Item purchase")}},
+    ]
  }
 ]

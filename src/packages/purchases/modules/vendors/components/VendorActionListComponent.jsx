70c4e0e487a683457ffa3f28b893d17c44a31b9d
paullaster <paullasterokoth98@gmail.com>
2024-01-28 02:27:40 +0300

    refactor: refactor ui

--- src/packages/purchases/modules/vendors/components/VendorActionListComponent.jsx
+++ src/packages/purchases/modules/vendors/components/VendorActionListComponent.jsx
@@ -1,12 +1,13 @@
 import VendorActionList from "./VendorActionList";
 import MenuPopup from "./MenuPopup";
+import { Button } from '@/components';
 
 const VendorActionListComponent = () => {
   return (
     <div>
         {
             VendorActionList.map((action) => {
-                return action.hasList ? <MenuPopup action={action}/> : ''
+                return action.hasList ? <MenuPopup action={action}/> : <Button></Button>
             })
         }
     </div>

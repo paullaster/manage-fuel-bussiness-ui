7da564f30fd4a4f464901ddc1bb7dd4da69a5d13
paullaster <paullasterokoth98@gmail.com>
2024-01-28 02:16:34 +0300

    refactor: refactor ui

--- src/packages/purchases/modules/vendors/components/MenuPopup.jsx
+++ src/packages/purchases/modules/vendors/components/MenuPopup.jsx
@@ -2,10 +2,11 @@ import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
 import { Button } from '@/components';
+import { v4 as uuidv4 } from 'uuid';
 
 const MenuPopup = () => {
   return (
-    <PopupState variant="popover" popupId="demo-popup-menu">
+    <PopupState variant="popover" popupId={uuidv4()}>
 
     </PopupState>
   )

b5023124fceb3797f6c5e60f57022d3de92b636f
paullaster <paullasterokoth98@gmail.com>
2024-01-27 23:50:54 +0300

    refactor: refactor ui

--- src/packages/purchases/modules/vendors/components/VendorList.jsx
+++ src/packages/purchases/modules/vendors/components/VendorList.jsx
@@ -1,102 +1,109 @@
 import Box from '@mui/material/Box';
-import { GridActionsCellItem } from "@mui/x-data-grid"; 
+import { GridActionsCellItem } from "@mui/x-data-grid";
 import { DataTable } from '@/components';
 import { useEffect, useMemo, useState } from 'react';
 import { fetchVendorsList } from '../../../actions';
 import { generator } from '@/utils/';
+import { v4 as uuidv4 } from 'uuid';
 
 const VendorList = () => {
     const [rows, setRows] = useState([]);
 
-  const columns = useMemo(() =>[
-    {
-        field: 'vendor_name',
-        headerName: 'name',
-        width: 300,
-        headerAlign: 'center',
-        sortable: true,
-        hideable: false,
-        editable: false,
-    },
-    {
-        field: 'vendor_email',
-        headerName: 'email ',
-        width: 200,
-        headerAlign: 'center',
-        sortable: false,
-        hideable: false,
-        editable: false,
-    },
-    {
-        field: 'vendor_phone',
-        headerName: 'phone',
-        width: 200,
-        headerAlign: 'center',
-        sortable: false,
-        hideable: false,
-        editable: false,
-    },
-    {
-        field: 'company_name',
-        headerName: 'company name',
-        width: 300,
-        headerAlign: 'center',
-        sortable: true,
-        hideable: false,
-        editable: false,
-    },
-    {
-        field: 'website',
-        headerName: 'website',
-        width: 200,
-        headerAlign: 'center',
-        sortable: false,
-        editable: false,
-        hideable: false
-    },
-    {
-        field: 'actions',
-        headerName: 'actions',
-        type: 'actions',
-        width: 200,
-        headerAlign: 'center',
-        sortable: false,
-        hideable: false,
-        editable: false,
-        getActions: (params) => {
-            return [
-                <GridActionsCellItem 
-                />
-            ]
+    const columns = useMemo(() => [
+        {
+            field: 'vendor_name',
+            headerName: 'name',
+            width: 300,
+            headerAlign: 'center',
+            sortable: true,
+            hideable: false,
+            editable: false,
+        },
+        {
+            field: 'vendor_email',
+            headerName: 'email ',
+            width: 200,
+            headerAlign: 'center',
+            sortable: false,
+            hideable: false,
+            editable: false,
+        },
+        {
+            field: 'vendor_phone',
+            headerName: 'phone',
+            width: 200,
+            headerAlign: 'center',
+            sortable: false,
+            hideable: false,
+            editable: false,
+        },
+        {
+            field: 'company_name',
+            headerName: 'company name',
+            width: 300,
+            headerAlign: 'center',
+            sortable: true,
+            hideable: false,
+            editable: false,
+        },
+        {
+            field: 'website',
+            headerName: 'website',
+            width: 200,
+            headerAlign: 'center',
+            sortable: false,
+            editable: false,
+            hideable: false
+        },
+        {
+            field: 'actions',
+            headerName: 'actions',
+            type: 'actions',
+            width: 200,
+            headerAlign: 'center',
+            sortable: false,
+            hideable: false,
+            editable: false,
+            getActions: (params) => {
+                return [
+                    <GridActionsCellItem
+                        key={uuidv4()}
+                        icon={<MdOutlineSaveAlt />}
+                        label="View"
+                        onClick={() => {
+                            handleViewClick(params)
+                        }}
+                    />
+                ]
+            }
+        },
+    ], []);
+
+    useEffect(() => {
+        fetchVendorsList()
+            .then((res) => {
+                console.log(res.vendors.results);
+                for (const vendor of generator(res.vendors.results)) {
+                    vendor.id = vendor.vendor_id;
+                    setRows((prev) => [...prev, vendor]);
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+
+        return () => {
+
         }
-    },
-  ], []);
-
-  useEffect(() => {
-    fetchVendorsList()
-    .then((res) => {
-        console.log(res.vendors.results);
-        for(const vendor of generator(res.vendors.results)) {
-            vendor.id = vendor.vendor_id;
-            setRows((prev) => [...prev, vendor]);
-        }
-    })
-    .catch((error) => {
-        console.log(error);
-    });
-
-    return () => {
-
-    }
-  }, []);
-  return (
-    <Box>
-        <DataTable 
-        columns={columns}
-        rows={rows}
-        />
-    </Box>
-  )
+    }, []);
+    return (
+        <Box>
+            <DataTable
+                columns={columns}
+                rows={rows}
+            />
+        </Box>
+    )
 }
 
 export default VendorList

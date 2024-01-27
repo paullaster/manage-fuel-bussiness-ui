3fd9c48c741241f0d3c57e9ecab1f29c2b545dd0
paullaster <paullasterokoth98@gmail.com>
2024-01-28 01:22:38 +0300

    refactor: refactor ui

--- src/packages/purchases/modules/vendors/components/VendorPageComponent.jsx
+++ src/packages/purchases/modules/vendors/components/VendorPageComponent.jsx
@@ -1,11 +1,18 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom"
+import { fetchVendorsList } from "../../../actions";
 
 const VendorPageComponent = () => {
   const { vendorId } = useParams();
 
   useEffect(() => {
-    
+    fetchVendorsList({vendor_id: vendorId})
+    .then((res) => {
+
+    })
+    .catch((error) => {
+        
+    })
   }, [vendorId]);
   return (
     <div>VendorPageComponent</div>

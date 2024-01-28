0d66645495faeae0fd5fe7c7db4b21abde694575
paullaster <paullasterokoth98@gmail.com>
2024-01-28 12:46:16 +0300

    refactor: refactor ui

--- src/packages/purchases/modules/vendors/components/VendorTransactionsComponent.jsx
+++ src/packages/purchases/modules/vendors/components/VendorTransactionsComponent.jsx
@@ -2,12 +2,16 @@ import Tab from '@mui/material/Tab';
 import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
-
+import { useState } from 'react';
+import Box from '@mui/material/Box'
 
 const VendorTransactionsComponent = () => {
+    const [value, setValue] = useState('1');
   return (
     <div>
-        
+        <TabContext value={value}>
+
+        </TabContext>
     </div>
   )
 }

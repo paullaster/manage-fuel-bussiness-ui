cd4f7ceb23da58092c730bfdaa7511a3bbb89909
paullaster <paullasterokoth98@gmail.com>
2024-01-28 13:18:43 +0300

    refactor: refactor ui

--- src/packages/purchases/modules/vendors/components/VendorTransactionsComponent.jsx
+++ src/packages/purchases/modules/vendors/components/VendorTransactionsComponent.jsx
@@ -4,6 +4,25 @@ import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
 import { useState } from 'react';
 import Box from '@mui/material/Box';
+import { v4 as uuidv4 } from 'uuid';
+
+
+const TabItems = [
+    {
+        key: uuidv4(),
+        imageUrl: 'http://dummy.image.com',
+        btnCaption: 'New fuel purchase',
+        additionalText: 'There is no fuel purchase for this vendor yet. Create a new one now.'
+    },
+    {
+        key: uuidv4(),
+        imageUrl: 'http://dummy.image.com',
+        btnCaption: 'New item purchase',
+        additionalText: 'There is no item purchase for this vendor yet. Create a new one now.'
+    },
+
+];
+
 
 const VendorTransactionsComponent = () => {
     const [value, setValue] = useState('1');
@@ -24,7 +43,7 @@ const VendorTransactionsComponent = () => {
                     </TabList>
                 </Box>
                 {
-                    
+
                 }
             </TabContext>
         </div>

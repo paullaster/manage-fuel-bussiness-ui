f76616ab7429bee4927f5a8c05aebe95481c6ee1
paullaster <paullasterokoth98@gmail.com>
2024-01-28 10:25:48 +0300

    refactor: refactor ui

--- src/packages/purchases/modules/vendors/components/VendorProfileComponent.jsx
+++ src/packages/purchases/modules/vendors/components/VendorProfileComponent.jsx
@@ -3,9 +3,21 @@ import React from 'react'
 const VendorProfileComponent = ({vendor}) => {
   return (
     <div>
-        
+        {vendor.map((prop) => {
+            console.log(prop)
+            if (prop === 'addresses'){
+                return (
+                    <VendorAddress key={prop.address_id} address={prop.}/>
+                )
+            }
+        })}
     </div>
   )
 }
 
 export default VendorProfileComponent
+
+
+const VendorAddress = ({address}) => {
+
+}

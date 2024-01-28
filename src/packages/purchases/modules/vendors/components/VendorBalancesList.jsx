6b17523cdfafaaf807342bb37edc992df38a2b27
paullaster <paullasterokoth98@gmail.com>
2024-01-28 09:48:28 +0300

    refactor: refactor ui

--- src/packages/purchases/modules/vendors/components/VendorBalancesList.jsx
+++ src/packages/purchases/modules/vendors/components/VendorBalancesList.jsx
@@ -1,7 +1,17 @@
 import { v4 as uuidv4 } from 'uuid';
+
 export default 
 [
     {
         key: uuidv4(),
-    }
+        cap: 'Overdue'
+    },
+    {
+        key: uuidv4(),
+        cap: 'Open'
+    },
+    {
+        key: uuidv4(),
+        cap: 'Paid'
+    },
 ]

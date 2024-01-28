aaaf74c76cb3e042aff3c2f64e911d742fc78e37
paullaster <paullasterokoth98@gmail.com>
2024-01-28 09:50:28 +0300

    refactor: refactor ui

---
+++ src/packages/purchases/modules/vendors/components/VendorBalancesListComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const VendorBalancesListComponent = () => {
+  return (
+    <div>VendorBalancesListComponent</div>
+  )
+}
+
+export default VendorBalancesListComponent

28873b4644f320cca33e8601d71f96f28498696a
paullaster <paullasterokoth98@gmail.com>
2024-01-28 12:49:01 +0300

    refactor: refactor ui

--- src/packages/purchases/modules/vendors/components/VendorTransactionsComponent.jsx
+++ src/packages/purchases/modules/vendors/components/VendorTransactionsComponent.jsx
@@ -8,12 +8,20 @@ import Box from '@mui/material/Box';
 const VendorTransactionsComponent = () => {
     const [value, setValue] = useState('1');
     const handleChange = (event, newValue) => {
+
         setValue(newValue);
     };
     return (
         <div>
             <TabContext value={value}>
-
+                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                    <TabList
+                    onChange={handleChange} 
+                    aria-label="vendor tabs"
+                    >
+                        
+                    </TabList>
+                </Box>
             </TabContext>
         </div>
     )

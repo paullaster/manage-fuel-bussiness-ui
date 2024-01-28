d4cb2a348d82ae803e078c1ac2c1de5e9507a698
paullaster <paullasterokoth98@gmail.com>
2024-01-28 13:19:08 +0300

    refactor: refactor ui

--- src/packages/purchases/modules/vendors/components/VendorTransactionsComponent.jsx
+++ src/packages/purchases/modules/vendors/components/VendorTransactionsComponent.jsx
@@ -43,7 +43,7 @@ const VendorTransactionsComponent = () => {
                     </TabList>
                 </Box>
                 {
-
+                    
                 }
             </TabContext>
         </div>

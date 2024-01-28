d287f007fd23e830bb4f6028ced7c5c50100da38
paullaster <paullasterokoth98@gmail.com>
2024-01-28 04:08:32 +0300

    refactor: refactor ui

--- src/packages/purchases/modules/vendors/components/BioAndBalancesComponent.jsx
+++ src/packages/purchases/modules/vendors/components/BioAndBalancesComponent.jsx
@@ -2,6 +2,7 @@ import { useCallback } from "react";
 
 const BioAndBalancesComponent = ({bio}) => {
   const getNameInitials = useCallback(() => {
+    console.log(bio)
     const nameArr = bio['name']?.split(' ');
     const initials = nameArr[0].charAt(0) + nameArr[1].charAt(0);
     return initials;

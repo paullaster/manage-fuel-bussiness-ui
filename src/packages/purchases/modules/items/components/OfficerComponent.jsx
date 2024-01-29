2677abc4e0bbaf5650928cb3e542a9ef2d0fa807
paullaster <paullasterokoth98@gmail.com>
2024-01-29 06:48:40 +0300

    update purchase items payload

--- src/packages/purchases/modules/items/components/OfficerComponent.jsx
+++ src/packages/purchases/modules/items/components/OfficerComponent.jsx
@@ -1,16 +1,18 @@
-import { AutocompleteComponent } from '@/components';
+import { AutocompleteComponent, DatePickerComponent } from '@/components';
 import DivisionTopBar from "../../../shared/components/DivisionTopBar";
-import { useState } from 'react';
 
 const OfficerComponent = ({ handleSelectedOficer, officers }) => {
     return (
         <div className='itemOfficer'>
-            <DivisionTopBar sectionTitle="Officer details" />
+            <DivisionTopBar sectionTitle="Officer details and Purchase date" />
             <AutocompleteComponent
                 list={officers}
                 label={'Select Officer'}
                 keyField={'name'}
                 handleOnchange={handleSelectedOficer}
+            />
+            <DatePickerComponent 
+                label='Purchase date'
             />
         </div>
     )

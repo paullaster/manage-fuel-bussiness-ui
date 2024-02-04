cea236be40a13b9aa7319b7f375560aeb5b8c448
paullaster <paullasterokoth98@gmail.com>
2024-02-04 08:42:13 +0300

    update landing

--- src/components/shared/DatePickerComponent.jsx
+++ src/components/shared/DatePickerComponent.jsx
@@ -3,14 +3,16 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
-const DatePickerComponent = ({setValue, label = 'mm/dd/yyyy' }) => {
+const DatePickerComponent = ({value = '', setValue, label = 'mm/dd/yyyy' }) => {
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DemoContainer components={['DatePicker']}>
         <DatePicker
           label={label}
-          onChange={setValue} />
+          onChange={setValue} 
+          value={value}
+          />
       </DemoContainer>
     </LocalizationProvider>
   )

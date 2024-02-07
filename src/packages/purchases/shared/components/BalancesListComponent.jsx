25712607e6ab5a15dc5f8adce3a1a1e88066d90e
paullaster <paullasterokoth98@gmail.com>
2024-02-07 19:02:35 +0300

    update vendor fetching

--- src/packages/purchases/shared/components/BalancesListComponent.jsx
+++ src/packages/purchases/shared/components/BalancesListComponent.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
-const BalancesListComponent = () => {
-  return (
-    <div>BalancesListComponent</div>
-  )
+const BalancesListComponent = ({currency = 'KES'}) => {
+    return (
+        <>
+            {
+                VendorBalancesList.map((bal) => {
+                    return (
+                        <div key={bal.key}>
+                            <p className="currencyAndBal">{currency} {bal.amount}</p>
+                            <p className="balanceCap">{bal.cap}</p>
+                        </div>
+                    )
+                })
+            }
+        </>
+    )
 }
 
 export default BalancesListComponent

e641ea47cb96bb14156aa81452ad74897f8e3951
paullaster <paullasterokoth98@gmail.com>
2024-02-08 04:11:17 +0300

    create single purchase item view

--- src/packages/purchases/shared/components/Report/ReportBillInformation.jsx
+++ src/packages/purchases/shared/components/Report/ReportBillInformation.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { usePurchasesState } from "../../../Context"
 
 const ReportBillInformation = () => {
+
+  const {bill} =usePurchasesState();
   return (
-    <div>ReportBillInformation</div>
+    <div>
+        <p><span>Bill Numner  </span> <span>{bill.bill_number || 'BILL-001'}</span></p>
+        <p><span>Order Number</span> {bill.order_number || 'ORDE34'}</p>
+        <p><span>Order Number</span> {bill.po_number || 'ORDE34'}</p>
+        <p><span>Inovice Number</span> {bill.invoice_number || 'ORDE34'}</p>
+        <p><span>Bill Date</span> {bill.purchase_date || ''}</p>
+    </div>
   )
 }
 

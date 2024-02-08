d346802b11228b6e1b6d95499b0c45879024e01a
paullaster <paullasterokoth98@gmail.com>
2024-02-08 04:59:52 +0300

    create single purchase item view

--- src/packages/purchases/shared/components/Report/ReportItemsInformation.jsx
+++ src/packages/purchases/shared/components/Report/ReportItemsInformation.jsx
@@ -1,7 +1,91 @@
 import { DataTable } from '@/components';
 import Box from '@mui/material/Box';
+import { useMemo } from 'react';
 
 const ReportItemsInformation = () => {
+    const columns = useMemo(() => [
+        {
+            field: 'vendor_name',
+            headerName: 'Name',
+            width: 300,
+            headerAlign: 'center',
+            sortable: true,
+            hideable: false,
+            editable: false,
+            align: 'center',
+        },
+        {
+            field: 'vendor_email',
+            headerName: 'Email ',
+            width: 200,
+            headerAlign: 'center',
+            sortable: false,
+            hideable: false,
+            editable: false,
+            align: 'center',
+        },
+        {
+            field: 'vendor_phone',
+            headerName: 'Phone',
+            width: 200,
+            headerAlign: 'center',
+            sortable: false,
+            hideable: false,
+            editable: false,
+            align: 'center',
+        },
+        {
+            field: 'company_name',
+            headerName: 'Company name',
+            width: 300,
+            headerAlign: 'center',
+            sortable: true,
+            hideable: false,
+            editable: false,
+            align: 'center',
+        },
+        {
+            field: 'website',
+            headerName: 'Website',
+            width: 200,
+            headerAlign: 'center',
+            sortable: false,
+            editable: false,
+            hideable: false,
+            align: 'center',
+        },
+        {
+            field: 'actions',
+            headerName: 'Actions',
+            type: 'actions',
+            width: 200,
+            headerAlign: 'center',
+            sortable: false,
+            hideable: false,
+            editable: false,
+            align: 'center',
+            getActions: (params) => {
+                return [
+                    <GridActionsCellItem
+                        key={uuidv4()}
+                        icon={<MdOutlineVisibility size={20} />}
+                        label="View"
+                        onClick={() => {
+                            handleViewClick(params)
+                        }}
+                    />,
+                    <GridActionsCellItem
+                        key={uuidv4()}
+                        icon={<MdDelete size={20} />}
+                        label="Delete"
+                        onClick={() => {
+                            handleDelete(params)
+                        }}
+                    />
+                ]
+            }
+        },
+    ], []);
   return (
     <Box>
 

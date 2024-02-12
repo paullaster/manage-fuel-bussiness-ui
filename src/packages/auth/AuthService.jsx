6f8dc8910fb8f1353bb484eaa99d3064e40c4901
paullaster <paullasterokoth98@gmail.com>
2024-02-12 04:02:35 +0300

    adding auth

---
+++ src/packages/auth/AuthService.jsx
@@ -0,0 +1,21 @@
+import WebStorage from "../../utils/WebStorage";
+import { APPNAME } from "../../environments";
+class AuthService {
+    accessToken = null;
+    refreshtoken = null;
+    user = null;
+
+    constructor() {
+        this.accessToken = WebStorage.GetFromWebStorage('session', `${APPNAME}_accessToken`);
+        this.refreshtoken = WebStorage.GetFromWebStorage('local', `${APPNAME}_refreshToken`);
+
+    }
+    Login(accessToken, refreshToken) {
+        this.accessToken = accessToken;
+        this.refreshtoken = refreshToken;
+        WebStorage.RemoveFromStorage('session', `${APPNAME}_accessToken`);
+        WebStorage.RemoveFromStorage('local', `${APPNAME}_refreshToken`);
+        WebStorage.storeToWebDB('session', `${APPNAME}_accessToken`, accessToken);
+        WebStorage.storeToWebDB('local', `${APPNAME}_refreshToken`, refreshToken,)
+    }
+}

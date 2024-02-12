b432b5f597d73c9531cb0ecc88b374d4e1ec565a
paullaster <paullasterokoth98@gmail.com>
2024-02-12 04:13:19 +0300

    adding auth

--- src/packages/auth/AuthService.jsx
+++ src/packages/auth/AuthService.jsx
@@ -16,8 +16,11 @@ class AuthService {
         WebStorage.RemoveFromStorage('session', `${APPNAME}_accessToken`);
         WebStorage.RemoveFromStorage('local', `${APPNAME}_refreshToken`);
         WebStorage.storeToWebDB('session', `${APPNAME}_accessToken`, accessToken);
-        WebStorage.storeToWebDB('local', `${APPNAME}_refreshToken`, refreshToken,);
+        WebStorage.storeToWebDB('local', `${APPNAME}_refreshToken`, refreshToken);
 
-        
     }
 }
+
+Object.freeze(AuthService);
+
+export default new AuthService();

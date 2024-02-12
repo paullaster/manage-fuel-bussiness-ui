6d8d242f825b22a1609a4c27fb345b2d8355203c
paullaster <paullasterokoth98@gmail.com>
2024-02-12 05:30:51 +0300

    adding auth

--- src/packages/dashboard/views/Dashboard.jsx
+++ src/packages/dashboard/views/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import purchases from "../../purchases";
 import { useGlobalDispatcher, useGlobalState } from "@/store";
 import { useEffect, useMemo } from "react";
@@ -7,43 +7,46 @@ import { RandomCodeGenerator } from "@/utils";
 
 const Dashboard = () => {
 
-   const appStateDispatcher = useGlobalDispatcher();
-   const appState = useGlobalState();
-   const location = useLocation();
+  const appStateDispatcher = useGlobalDispatcher();
+  const { links, isAuthenticated } = useGlobalState();
+  const location = useLocation();
+  const navigate = useNavigate();
 
-   const transactionCode  = useMemo(() => {
+  const transactionCode = useMemo(() => {
     const locationArray = location.pathname.split('/');
-    if(locationArray.includes('purchases') && (locationArray.includes('fuel') || locationArray.includes('item')) && locationArray.includes('create')) {
+    if (locationArray.includes('purchases') && (locationArray.includes('fuel') || locationArray.includes('item')) && locationArray.includes('create')) {
       return RandomCodeGenerator();
     }
-   }, [location])
+  }, [location])
 
-   useEffect(() => {
-      appStateDispatcher({
-        type: "PURCHASE_CODE",
-        payload: transactionCode,
-      })
-      appStateDispatcher({
-        type: "LINKS",
-        payload: purchases.links
-      });
-      /**
-       * @TODO take the transanction code state to purchase item of the dashboard instead of having it affected the entire dashboard that is made of many heavy packeges
-       * - this is to be done for code refactoring and optimization
-       */
-   },[transactionCode]);
+  useEffect(() => {
+    appStateDispatcher({
+      type: "PURCHASE_CODE",
+      payload: transactionCode,
+    })
+    appStateDispatcher({
+      type: "LINKS",
+      payload: purchases.links
+    });
+    /**
+     * @TODO take the transanction code state to purchase item of the dashboard instead of having it affected the entire dashboard that is made of many heavy packeges
+     * - this is to be done for code refactoring and optimization
+     */
+  }, [transactionCode]);
 
-
+  useEffect(() => {
+    if (!isAuthenticated) navigate('/account/login');
+  }, [isAuthenticated]);
 
   return (
-  <>
+    <>
       <header className='header'>
-        <Navigation links={appState.links} />
+        <Navigation links={links} />
       </header>
       <main className='main'>
-          <div className="container">
-            <Outlet />
-          </div>
+        <div className="container">
+          <Outlet />
+        </div>
       </main >
       <footer>
         <Footer className='footer' />

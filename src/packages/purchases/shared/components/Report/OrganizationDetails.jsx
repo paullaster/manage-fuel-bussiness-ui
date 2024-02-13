65167476f78889dddda1f9573fef218d3f492acd
paullaster <paullasterokoth98@gmail.com>
2024-02-13 06:51:10 +0300

    update auth

--- src/packages/purchases/shared/components/Report/OrganizationDetails.jsx
+++ src/packages/purchases/shared/components/Report/OrganizationDetails.jsx
@@ -4,7 +4,7 @@ import { APPNAME } from "@/environments";
 const orgData = WebStorage.GetFromWebStorage('session', `${APPNAME}_ORG_DATA`);
 const OrganizationDetails = () => {
   return (
-    <div>
+    <div className="organizationDetails">
         <p>{orgData.company_name }</p>
         <p>{orgData.location }</p>
         <p>Tax Number: {orgData.tax_number }</p>

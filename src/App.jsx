287acb11ab063434143b1b14146f274438f9c678
paullaster <paullasterokoth98@gmail.com>
2024-02-25 23:40:01 +0300

    update on a new a workstations

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,24 @@ import { useEffect, useState } from 'react';
 import { Hide } from './utils';
 
 const App = () => {
-  
-  const [auth, setAuth] = useState({user: null, isAuthenticated: false});
+
+  const [auth, setAuth] = useState({ user: null, isAuthenticated: false });
   const navigate = useNavigate();
   const location = useLocation();
 
-if (Hide('/account') && auth.isAuthenticated) navigate('/dasboard');
+  if (Hide('/account') && auth.isAuthenticated) navigate('/dasboard');
 
-if (!auth.isAuthenticated && Hide('/dashboard') ) return navigate('/account/login');
+  if (!auth.isAuthenticated && Hide('/dashboard')) return navigate('/account/login');
 
   useEffect(() => {
 
-  },[auth, location])
+  }, [auth, location])
 
   return (
-    <AuthContext.Provider value={{account: auth, authSetter: setAuth}}>
-    <StateProvider>
-      <Outlet />
-    </StateProvider>
+    <AuthContext.Provider value={{ account: auth, authSetter: setAuth }}>
+      <StateProvider>
+        <Outlet />
+      </StateProvider>
     </AuthContext.Provider>
   )
 }

5668888507a792245f2ae7a2a1916ff4cf749c60
paullaster <paullasterokoth98@gmail.com>
2024-02-25 23:24:46 +0300

    update on a new a workstations

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate, useLocation } from 'react-router-dom';
 import { StateProvider } from './store';
 import { AuthContext } from './store';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const App = () => {
   
   const [auth, setAuth] = useState({user: null, isAuthenticated: false});
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+
+  },[auth, location])
 
   return (
     <AuthContext.Provider value={{user: null, isAuthenticated: false}}>

5f9c6f2220ee395b6d2e167a364c3d1740dfeb5c
paullaster <paullasterokoth98@gmail.com>
2024-03-09 17:16:07 +0300

    update purchases

--- src/utils/DateFormatter.jsx
+++ src/utils/DateFormatter.jsx
@@ -1,8 +1,7 @@
 export const YearMonthDate = (date) => {
     const dateObj = new Date(date);
     /*${dateObj.getHours()}:${dateObj.getMinutes() < 10 ? prefixZero(dateObj.getMinutes()) : dateObj.getMinutes() }:${dateObj.getSeconds() < 10 ? prefixZero(dateObj.getSeconds()) : dateObj.getSeconds()}*/
-    const formattedDate = `
-    ${dateObj.getFullYear()}-${dateObj.getMonth() < 10 ? prefixZero(dateObj.getMonth() + 1): dateObj.getMonth() + 1 }-${dateObj.getDate() < 10 ? prefixZero(dateObj.getDate() + 1) : dateObj.getDate() + 1}`;
+    const formattedDate = `${dateObj.getFullYear()}-${dateObj.getMonth() < 10 ? prefixZero(dateObj.getMonth() + 1): dateObj.getMonth() + 1 }-${dateObj.getDate() < 10 ? prefixZero(dateObj.getDate() + 1) : dateObj.getDate() + 1}`;
     return formattedDate;
 }
 

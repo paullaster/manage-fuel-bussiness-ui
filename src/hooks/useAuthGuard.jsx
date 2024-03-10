fcbc32c7e8c2f1a08c2044707cfe8a9491098ed4
paullaster <paullasterokoth98@gmail.com>
2024-03-10 17:37:57 +0300

    reactor authentication and authorization

--- src/hooks/useAuthGuard.jsx
+++ src/hooks/useAuthGuard.jsx
@@ -1,20 +1,20 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import { AuthContext } from '../store';
 import AuthService from '../packages/auth/AuthService';
 
 const ProtectionMiddleware = ({redirectTo = '/account/login'}) => {
 
-    const { account, setAuth } = useContext(AuthContext);
+    const { account, authSetter } = useContext(AuthContext);
     const { isAuthenticated } = account;
 
     useEffect(() => {
         const handleCheckAuth = async () => {
-          setAuth({ user: AuthService.getUser(), isAuthenticated: AuthService.isLoggedIn() });
+            authSetter({ user: AuthService.getUser(), isAuthenticated: AuthService.isLoggedIn() });
         }
         handleCheckAuth();
       }, []);
-
+      console.log(isAuthenticated);
     if (isAuthenticated && window.location.pathname.includes('/account/')) {
         return <Navigate to={'/dashboard'}  replace />
     }
